928b07d3dddd921d2f79cbf6fc3f0781a8373a54
Michael Kearney <mkearney@ku.edu>
2016-08-16 19:31:29 -0500

    updated search tweets testthat

--- tests/testthat/test.searching.R
+++ tests/testthat/test.searching.R
@@ -5,28 +5,32 @@ library(rtweet)
 context("Search tweets")
 
 n <- 25
-srch <- search_tweets(
+x <- search_tweets(
   "twitter", n = n,
   lang = "en",
   verbose = FALSE)
 
-test_that("search_tweets returns two data frames", {
-  expect_equal(length(srch), 2)
-  expect_named(srch, c("tweets", "users"))
+test_that("search_tweets returns tweets data", {
+  expect_true(is.data.frame(x))
+  expect_named(x)
+  expect_true("status_id" %in% names(x))
 })
-
-tweets <- srch[["tweets"]]
-users <- srch[["users"]]
 
 test_that(paste0("tweets df contains ", n, " rows and 27 columns"), {
-  expect_equal(nrow(tweets), n)
-  expect_gt(ncol(tweets), 23)
-  expect_named(tweets)
-  expect_equal(unique(getElement(tweets, "lang")), "en")
+  expect_equal(nrow(x), n)
+  expect_gt(ncol(x), 23)
+  expect_named(x)
+  expect_equal(unique(getElement(x, "lang")), "en")
 })
 
-test_that(paste0("users df contains > 1 rows and 19 columns"), {
-  expect_gt(nrow(users), 0)
-  expect_gt(ncol(users), 15)
-  expect_named(users)
+test_that("search_tweets object contains users attribute", {
+  expect_true("users" %in% attributes(x))
+  expect_true(is.data.frame(attr(x, "users")))
+  expect_true(users_data(x))
+})
+
+test_that(paste0("users data contains > 1 rows and 19 columns"), {
+  expect_gt(nrow(users_data(x)), 0)
+  expect_gt(ncol(users_data(x)), 15)
+  expect_named(users_data(x))
 })
