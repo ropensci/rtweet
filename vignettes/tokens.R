230178cc540a0d97d1b32273de6b42b835829845
Michael Kearney <mkearney@ku.edu>
2016-08-18 19:53:22 -0500

    build vignette

---
+++ vignettes/tokens.R
@@ -0,0 +1,8 @@
+## ---- include = FALSE----------------------------------------------------
+knitr::opts_chunk$set(comment = "#>", collapse = TRUE)
+
+## ---- echo = TRUE, eval = FALSE------------------------------------------
+#  home_directory <- normalizePath("~/")
+#  file_name <- paste0(home_directory, "/", "twitter_tokens")
+#  save(twitter_tokens, file = file_name)
+
