ff16296c8e8f069a06607dbe54e861d949cf8670
Michael Kearney <mkearney@ku.edu>
2016-08-20 22:08:51 -0500

    added twitter_tokens to testthat

--- tests/testthat/test_stream_tweets.R
+++ tests/testthat/test_stream_tweets.R
@@ -1,4 +1,5 @@
 skip_on_cran()
+skip_on_travis()
 
 library(rtweet)
 
--- tests/testthat/test_twitter_token.R
+++ tests/testthat/test_twitter_token.R
@@ -4,12 +4,12 @@ library(rtweet)
 
 context("twitter_token")
 
-load("twitter_token")
+twitter_tokens <- readRDS("twitter_tokens")
 
 x <- search_tweets(
 	q = "twitter",
 	verbose = FALSE,
-	token = twitter_token)
+	token = twitter_tokens)
 
 test_that("search_tweets with twitter_token returns tweets data", {
   expect_equal(is.data.frame(x), TRUE)
@@ -20,4 +20,16 @@ test_that("search_tweets with twitter_token returns tweets data", {
 test_that("output contains > 10 rows and > 10 columns", {
   expect_gt(nrow(x), 10)
   expect_gt(ncol(x), 10)
+})
+
+test_that("search_tweets object contains users attribute", {
+	expect_true("users" %in% names(attributes(x)))
+	expect_true(is.data.frame(attr(x, "users")))
+	expect_true(is.data.frame(users_data(x)))
+})
+
+test_that("users data contains > 5 rows and > 5 columns", {
+	expect_named(users_data(x))
+	expect_gt(nrow(users_data(x)), 5)
+	expect_gt(ncol(users_data(x)), 5)
 })
