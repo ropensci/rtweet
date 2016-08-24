8c21230e109fd5fac29e632365aa9f6dc738dc66
Michael Kearney <mkearney@ku.edu>
2016-08-24 10:34:47 -0500

    added error tests to search_tweets

--- tests/testthat/test_search_tweets.R
+++ tests/testthat/test_search_tweets.R
@@ -5,11 +5,12 @@ test_that("search_tweets returns tweets data", {
 
 	n <- 3
 	token <- readRDS("twitter_tokens")
-	x <- search_tweets("twitter", n = n,
-		lang = "en", verbose = FALSE, token = token)
+	x <- search_tweets("tweet tweet", n = n,
+		lang = "en", verbose = TRUE, token = token)
 
   expect_equal(is.data.frame(x), TRUE)
   expect_named(x)
+  expect_true(all(x$lang == "en"))
   expect_true("status_id" %in% names(x))
   expect_equal(nrow(x), n)
   expect_gt(ncol(x), 23)
@@ -19,4 +20,16 @@ test_that("search_tweets returns tweets data", {
   expect_gt(nrow(users_data(x)), 0)
   expect_gt(ncol(users_data(x)), 15)
   expect_named(users_data(x))
+
+  expect_error(search_tweets(verbose = FALSE, token = token))
+  expect_error(search_tweets("tweet", token = "token"))
+  expect_error(search_tweets("stats", type = "all", token = token))
+})
+
+test_that("search_tweets fails", {
+	skip_on_cran()
+
+
+
+	expect
 })
