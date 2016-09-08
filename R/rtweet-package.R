8fe6732d8ea3174084734f6d44355c606606b08e
Michael Kearney <mkearney@ku.edu>
2016-09-08 16:54:06 -0500

    update vers num startup

--- R/rtweet-package.R
+++ R/rtweet-package.R
@@ -22,7 +22,7 @@
 NULL
 
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to rtweet v0.2.5!")
+  packageStartupMessage("Welcome to rtweet v0.2.6!")
 }
 
 .state <- new.env(parent = emptyenv())
