c27ae1927a49755050abaf18e325fdccaebaaacf
Michael Kearney <mkearney@ku.edu>
2016-09-21 17:48:50 -0500

    test fix

--- tests/testthat/test_tokens.R
+++ tests/testthat/test_tokens.R
@@ -5,13 +5,13 @@ test_that("get_tokens returns tokens", {
 	skip_on_cran()
 
 	token <- readRDS("twitter_tokens")
-	saveRDS(token, file = "~/twitter_tokens.rds")
+	saveRDS(token, file = ".httr_oauth")
 	tokens <- get_tokens()
 	expect_true(is.list(tokens))
 	expect_true("Token" %in% class(tokens[[1]]))
 
-	Sys.setenv(TWITTER_PAT = "~/twitter_tokens.rds")
 	lol <- search_tweets("lol")
 	expect_true(is.data.frame(lol))
 	expect_true(nrow(lol) > 80)
+	file.remove(".httr_oauth")
 })
