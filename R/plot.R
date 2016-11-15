2a49b18682ec273a5d4ea49d8442aabbd7422165
Michael Kearney <mkearney@ku.edu>
2016-11-14 22:55:34 -0600

    customize plot args

--- R/plot.R
+++ R/plot.R
@@ -10,6 +10,9 @@
 #' @param exclude Vector of regular expressions with which to
 #'   distinguish data.
 #' @param key Labels for filters. Defaults to actual filters.
+#' @param cols Colors for filters
+#' @param leg.x Location for plot text
+#' @param leg.y Location for plot text
 #' @param \dots Other arguments passed to plot function.
 #'
 #' @examples
@@ -28,7 +31,11 @@ ts_plot <- function(rt,
                     txt = "text",
                     filter = NULL,
                     exclude = NULL,
-                    key = NULL, ...) {
+                    key = NULL,
+                    cols = NULL,
+                    leg.x = NULL,
+                    leg.y = NULL,
+                    ...) {
   if (is.null(filter)) {
     filter <- ""
   }
@@ -62,7 +69,8 @@ ts_plot <- function(rt,
         col = "#f5f5f5", border = NA),
     panel.last = grid(4, 4, lty = grid.major, lwd = .75),
     grid(8, 8, lty = grid.minor, lwd = .5)))
-  axis(2, at = round(seq(ylim[1], ylim[2], length.out = 4), 0),
+  yaxis <- signif(seq(ylim[1], ylim[2], length.out = 4), 2)
+  axis(2, at = round(yaxis, 0),
     lwd = 2, col = "#666666",
     lwd.ticks = 2, col.ticks = "#444444")
   if (any(grepl("sec|min", by))) {
@@ -95,28 +103,38 @@ ts_plot <- function(rt,
       lwd.ticks = 2, col.ticks = "#444444")
   }
 
-  cols <- gg_cols(length(filter))
-  cols <- sample(cols)
-  for (i in seq_along(df)) {
-    lo <- mean(df[[i]]$freq, na.rm = TRUE) -
-      (sd(df[[i]]$freq, na.rm = TRUE) * .1)
-    if (all(
-      df[[i]]$freq[1] < lo,
-      df[[i]]$freq[NROW(df[[i]])] < lo)) {
-      df[[i]] <- df[[i]][2:(NROW(df[[i]]) - 1), ]
-    }
-    x <- df[[i]][, 1:2]
-    lines(x, lwd = 2.5, col = paste0(cols[i], "cc"))
+  if (is.null(cols)) {
+    cols <- gg_cols(length(filter))
+    cols <- sample(cols)
+  }
+
+  if (all(is.null(leg.x), is.null(leg.y))) {
     rnum <- runif(1, .01, .99)
     leg.x <- x[round(NROW(x) * rnum, 0), 1]
     rnum <- runif(1, .1, .9)
     leg.y <- x[round(NROW(x) * rnum, 0), 2]
-    leg.text <- unique(as.character(df[[i]][, 3]))
     leg.y <- (leg.y + 5) * runif(1, .7, 1.1)
     if (leg.y < ylim[1]) leg.y <- ylim[1]
     if (leg.y > ylim[2]) leg.y <- ylim[2]
-    text(leg.x, leg.y, labels = leg.text,
-      col = paste0(cols[i], "ff"), cex = runif(1, .95, 1.3))
+  } else {
+    leg.x <- as.POSIXct(x[round(NROW(x) * leg.x, 0), 1])
+  }
+  for (i in seq_along(df)) {
+    lo <- mean(df[[i]]$freq, na.rm = TRUE) +
+      (sd(df[[i]]$freq, na.rm = TRUE) * .2)
+    if (any(
+      df[[i]]$freq[1] < lo,
+      df[[i]]$freq[NROW(df[[i]])] < lo)) {
+      df[[i]] <- df[[i]][(length(filter) + 1):(NROW(
+        df[[i]]) - 1 - length(filter)), ]
+    }
+    x <- df[[i]][, 1:2]
+    lines(x, lwd = 2.5, col = paste0(cols[i], "cc"))
+
+
+    leg.text <- unique(as.character(df[[i]][, 3]))
+    text(leg.x[i], leg.y[i], labels = leg.text,
+      col = paste0(cols[i], "ff"), cex = 1.25)
   }
   par(op)
   invisible(do.call("rbind", df))
