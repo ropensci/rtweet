a7fd5260a1bf8643c68bc17a336d23c4670b4cb3
Michael Kearney <mkearney@ku.edu>
2016-12-06 14:00:45 -0600

    added follow_user function

--- R/tweet.R
+++ R/tweet.R
@@ -38,3 +38,47 @@ post_tweet <- function(status = "my first rtweet #rstats",
 	message("your tweet has been posted!")
 }
 
+
+
+
+
+
+#' follow_user
+#'
+#' @description Follows target twitter user.
+#'
+#' @param user Screen name or user id of target user.
+#' @param notify Logical indicating whether to enable notifications
+#'   for target user. Defaults to false.
+#' @param token OAuth token. By default \code{token = NULL}
+#'   fetches a non-exhausted token from an environment
+#'   variable tokens.
+#'
+#' @examples
+#' \dontrun{
+#' follow_user("BarackObama")
+#' }
+#' @export
+follow_user <- function(user, notify = FALSE, token = NULL) {
+
+  stopifnot(is.atomic(user), is.logical(notify))
+
+  token <- check_token(token)
+
+  query <- "friendships/create"
+
+  params <- list(
+    user_type = user,
+    notify = notify)
+
+  names(params)[1] <- .id_type(user)
+
+  url <- make_url(query = query, param = params)
+
+  r <- TWIT(get = FALSE, url, token)
+
+  if (r$status_code != 200) message("something didn't work")
+
+  r
+}
+
