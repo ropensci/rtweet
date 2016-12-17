5ef897ed977513873747191f3c65f2134b6c1db2
Michael Kearney <mkearney@ku.edu>
2016-12-17 12:36:39 -0600

    fixed name in get_followers test

--- tests/testthat/test_get_followers.R
+++ tests/testthat/test_get_followers.R
@@ -1,6 +1,6 @@
 context("get_followers")
 
-test_that("get_followers returns data frame with ids", {
+test_that("get_followers returns data frame with user_id", {
     skip_on_cran()
 
     token <- readRDS("twitter_tokens")
@@ -8,6 +8,6 @@ test_that("get_followers returns data frame with ids", {
 
     expect_true(is.data.frame(f))
     expect_true(identical(length(next_cursor(f)), 1L))
-    expect_named(f, "ids")
+    expect_named(f, "user_id")
     expect_gt(NROW(f), 9999)
 })
