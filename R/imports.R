1a35c30298df9cdc1582365ed967c52e910efb04
Michael Kearney <mkearney@ku.edu>
2017-01-07 01:04:10 -0600

    tibble documentation

--- R/imports.R
+++ R/imports.R
@@ -1,4 +1,9 @@
+#' tbl
+#'
+#' Imports tibble as_data_frame
+#'
+#' @param \dots Args passed to tibble as_data_frame
+#' @return Tibble data frame.
 #' @importFrom tibble as_data_frame
 #' @export
 tbl <- function(...) tibble::as_data_frame(...)
-
