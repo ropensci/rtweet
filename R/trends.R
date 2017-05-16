5c0d8c2d6159380af5e15c73885e21333224829b
Ben Dilday <ben.dilday.phd@gmail.com>
2017-05-16 08:37:32 -0400

    Support for trends/closest endpoint

--- R/trends.R
+++ R/trends.R
@@ -63,6 +63,71 @@ get_trends <- function(woeid = 1,
     gt
 }
 
+#' get_trends_closest
+#'
+#' @description Returns Twitter trends
+#'
+#' @param lat Numeric, Latitude in degrees
+#' @param long Numeric, Longitude in degrees
+#' @param exclude Logical, indicating whether or not to exclude
+#'   hashtags
+#' @param token OAuth token. By default \code{token = NULL} fetches a
+#'   non-exhausted token from an environment variable. Find instructions
+#'   on how to create tokens and setup an environment variable in the
+#'   tokens vignette (in r, send \code{?tokens} to console).
+#' @param parse Logical, indicating whether or not to parse return
+#'   trends data.
+#'
+#' @examples
+#' \dontrun{
+#'
+#' # Retrieve trends data for latitude, longitude near New York City
+#' nyc_trends <- get_trends_closest(lat = 40.7, long=-74.0, token=twitter_token)
+#'
+#' # Preview trends data
+#' nyc_trends
+#' }
+#'
+#' @return Trend data closest to a given location.
+#' @family trends
+#' @export
+get_trends_closest <- function(lat = NULL,
+                               long = NULL,
+                               exclude = FALSE,
+                               token = NULL,
+                               parse = TRUE) {
+
+  stopifnot(!is.null(lat), !is.null(long))
+  token <- check_token(token, query)
+
+  woeid <- trends_closest(lat, long, token)$woeid
+
+  get_trends(woeid = woeid, exclude = exclude, token = token, parse = parse)
+
+}
+
+#' Get closest available location to given Lat, Long
+#'
+#' @description Uses the trends/closest API endpoint to get the
+#'  the closest location to a given Latitude, Longitude pair. Returns
+#'  a data frame describing the location.
+#' @param lat Latitude in degrees.
+#' @param long Longitude in degrees.
+#' @param token OAuth token.
+#' @examples
+#' \dontrun{
+#' # New York is ~ 40.7N, 74.0W
+#' nyc_woeid <- trends_closest(40.7, -74.0, twitter_token)
+#' }
+trends_closest <- function(lat, long, token) {
+  query <- "trends/closest"
+  token <- check_token(token, query)
+  url <- make_url(query = query,
+                  param = list(lat=lat, long=long))
+  trd <- TWIT(get = TRUE, url, token)
+  trd <- from_js(trd)
+  trd
+}
 
 parse_trends <- function(x) {
     trends <- data.frame(x$trends[[1]],
@@ -141,3 +206,4 @@ parse_trends_available <- function(x) {
     names(p)[ncol(p)] <- "place_type"
     p
 }
+
