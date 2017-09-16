11faaae11e2b335d2bc5871e8e02a3dd8a51dfe8
Michael W. Kearney <kearneymw@missouri.edu>
2017-09-16 10:50:19 -0500

    code formatting

--- R/format_date.R
+++ R/format_date.R
@@ -6,9 +6,11 @@
 #' @noRd
 format_date2 <- function(created_at, tz = "UTC") {
   stopifnot(is.character(created_at))
-  as.POSIXct(created_at,
-             format = "%a %b %d %T %z %Y",
-             tz = tz)
+  as.POSIXct(
+    created_at,
+    format = "%a %b %d %T %z %Y",
+    tz = tz
+  )
 }
 
 format_date <- function(x, date = FALSE, tz = "UTC") {
@@ -22,25 +24,32 @@ format_date <- function(x, date = FALSE, tz = "UTC") {
   )
   if (any(is.null(o), all(is.na.quiet(o)))) {
     o <- tryCatch(as.POSIXct(
-      x, format = "%a %b %d %H:%M:%S %z %Y",
-      tz = tz, origin = "1970-01-01"),
+      x,
+      format = "%a %b %d %H:%M:%S %z %Y",
+      tz = tz,
+      origin = "1970-01-01"),
       error = function(e) return(NULL))
   }
   if (any(is.null(o), all(is.na.quiet(o)))) {
     o <- tryCatch(as.POSIXct(
-      x, format = "%a %b %d %H:%M:%S %z %Y"),
+      x,
+      format = "%a %b %d %H:%M:%S %z %Y"),
       error = function(e) return(NULL))
   }
   if (any(is.null(o), all(is.na.quiet(o)))) {
     curLocale <- Sys.getlocale("LC_TIME")
-    on.exit(Sys.setlocale("LC_TIME", curLocale),
-            add = TRUE)
+    on.exit(
+      Sys.setlocale("LC_TIME", curLocale),
+      add = TRUE
+    )
     Sys.setlocale("LC_TIME", "C")
 
     o <- tryCatch(as.POSIXct(
-      x, tz = tz,
+      x,
+      tz = tz,
       format = "%a, %d %b %Y %H:%M:%S +0000"),
-      error = function(e) return(NULL))
+      error = function(e) return(NULL)
+    )
   }
   if (any(is.null(o), all(is.na.quiet(o)))) {
     o <- tryCatch(as.POSIXct(
@@ -60,4 +69,4 @@ format_date <- function(x, date = FALSE, tz = "UTC") {
     o <- as.Date(o)
   }
   o
-  }
+}
