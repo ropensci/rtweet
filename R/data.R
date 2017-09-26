d4b1b35171ffd66f66b8e6a73da3d4f9f76be3a1
Michael W. Kearney <kearneymw@missouri.edu>
2017-09-26 12:40:48 -0500

    added emojis and langs data

---
+++ R/data.R
@@ -0,0 +1,27 @@
+#' emojis data
+#'
+#' This data comes from Unicode.org, 
+#' \url{http://unicode.org/emoji/charts/full-emoji-list.html}. The data are
+#' codes and descriptions of emojis.
+#' 
+#' @docType data
+#' @name emojis
+#' @format A tibble with two variables and 2,623 observations.
+#' @examples
+#' emojis
+"emojis"
+
+#' langs data
+#'
+#' This data comes from the Library of Congress, 
+#' \url{http://www.loc.gov/standards/iso639-2/ISO-639-2_utf-8.txt}. The data are
+#' descriptions and codes associated with internationally recognized languages.
+#' Variables include translations for each language represented as
+#' bibliographic, terminologic, alpha, english, and french.
+#' 
+#' @docType data
+#' @name langs
+#' @format A tibble with five variables and 486 observations.
+#' @examples
+#' langs
+"langs"
