b6b674fa95b1e5da69a21ad407f5c7d4fc03f3e5
Michael W. Kearney <kearneymw@missouri.edu>
2017-09-27 18:58:53 -0500

    added get_retweeters, updated docs

--- R/retweets.R
+++ R/retweets.R
@@ -1,31 +1,49 @@
 #' GET statuses/retweets/:id
 #'
+#' Returns a collection of the 100 most recent retweets of the Tweet
+#' specified by the id parameter. Twitter's API is currently limited to 100
+#' or fewer retweeters.
+#' 
 #' @param id required The numerical ID of the desired status.
-#' @param count optional Specifies the number of records to retrieve.
+#' @param n optional Specifies the number of records to retrieve.
 #'   Must be less than or equal to 100.
+#' @param parse Logical indicating whether to convert the response object into
+#'   an R list. Defaults to TRUE.
+#' @param ... Other arguments used as parameters in the query sent to
+#'   Twitter's rest API, for example, \code{trim_user = TRUE}.
 #' @param token OAuth token. By default \code{token = NULL} fetches a
 #'   non-exhausted token from an environment variable. Find
 #'   instructions on how to create tokens and setup an environment
 #'   variable in the tokens vignette (in r, send \code{?tokens} to
 #'   console).
 #' @return data
-statuses_retweets <- function(id, count = 100, token = NULL) {
-  query <- "statuses/retweets"
-  params <- list(id = id,
-                 count = count)
+get_retweets <- function(status_id, n = 100, parse = TRUE, ..., token = NULL) {
+  query <- "statuses/retweets/:id"
+  params <- list(
+    id = status_id,
+    count = n,
+    ...
+  )
   token <- check_token(token, query)
   url <- make_url(query = query, param = params)
   r <- httr::GET(url, token)
-  from_js(r)
+  if (parse) {
+    r <- from_js(r)
+    r <- as_retweets(r)
+    r <- as.data.frame(r)
+  }
+  r
 }
 
 
 #' GET statuses/retweeters/ids
 #'
 #' Returns a collection of up to 100 user IDs belonging to users who
-#'   have retweeted the Tweet specified by the id parameter.
+#' have retweeted the Tweet specified by the id parameter.
 #' 
-#' @param id required The numerical ID of the desired status.
+#' @param status_id required The status ID of the desired status.
+#' @param n Specifies the number of records to retrieve.
+#'   Best if intervals of 100.
 #' @param cursor semi-optional Causes the list of IDs to be broken into pages
 #'   of no more than 100 IDs at a time. The number of IDs returned is not
 #'   guaranteed to be 100 as suspended users are filtered out after
@@ -38,6 +56,8 @@ statuses_retweets <- function(id, count = 100, token = NULL) {
 #'   can be returned in a single cursored collection. Using the count
 #'   parameter with this method will not provide segmented cursors for
 #'   use with this parameter.
+#' @param parse Logical indicating whether to convert the response object into
+#'   an R list. Defaults to TRUE.
 #' @param token OAuth token. By default \code{token = NULL} fetches a
 #'   non-exhausted token from an environment variable. Find
 #'   instructions on how to create tokens and setup an environment
@@ -45,14 +65,88 @@ statuses_retweets <- function(id, count = 100, token = NULL) {
 #'   console).
 #' @return data
 #' @export
-statuses_retweeters <- function(id, cursor = "-1", token = NULL) {
-  query <- "statuses/retweets"
-  params <- list(id = id,
-                 cursor = cursor,
-                 stringify_ids = TRUE)
+get_retweeters <- function(status_id,
+                           n = 100,
+                           cursor = "-1",
+                           parse = TRUE,
+                           token = NULL) {
+  get_retweeters_(
+    status_id = status_id, n = n, cursor = cursor, parse = parse, token = token
+  )
+}
+
+get_retweeters_ <- function(status_id,
+                            n = 100,
+                            cursor = "-1",
+                            parse = TRUE,
+                            token = NULL) {
+  stopifnot(is_n(n))
+  n <- ceiling(n / 100L)
+  r <- vector("list", n)
+  for (i in seq_along(r)) {
+    r[[i]] <- get_retweeters_call(
+      status_id = status_id, cursor = cursor, token = token
+    )
+    cursor <- attr(r[[i]], "next_cursor")
+    if (is.null(cursor) || length(cursor) == 0 || identical(cursor, "0")) {
+      break
+    }
+  }
+  if (parse) {
+    r <- do.call("rbind", r)
+    attr(r, "next_cursor") <- cursor
+  }
+  r
+}
+
+get_retweeters_call <- function(status_id,
+                                cursor = "-1",
+                                parse = TRUE,
+                                token = NULL) {
+  query <- "statuses/retweeters/ids"
+  stopifnot(is.character(status_id), is.character(cursor))
+  params <- list(
+    id = status_id,
+    count = 100,
+    cursor = cursor,
+    stringify_ids = TRUE
+  )
   token <- check_token(token, query)
   url <- make_url(query = query, param = params)
   r <- httr::GET(url, token)
-  from_js(r)
+  if (parse) {
+    r <- from_js(r)
+    if (has_name_(r, "next_cursor_str")) {
+      next_cursor <- r$next_cursor_str
+    } else {
+      next_cursor <- NULL
+    }
+    r <- as_retweeters(r)
+    r <- as.data.frame(r)
+    attr(r, "next_cursor") <- next_cursor
+  } else {
+    rr <- httr::content(r)
+    if (has_name_(rr, "next_cursor_str")) {
+      next_cursor <- rr$next_cursor_str
+    } else {
+      next_cursor <- NULL
+    }
+  }
+  r
 }
 
+as_retweeters <- function(x) {
+  structure(x, class = "retweeters")
+}
+
+as.data.frame.retweeters <- function(x) {
+  if (has_name_(x, "ids")) {
+    x <- data.frame(
+      user_id = x$ids,
+      stringsAsFactors = FALSE
+    )
+  } else {
+    x <- data.frame()
+  }
+  x
+}
---
+++ cran-comments.md
@@ -0,0 +1,12 @@
+## Test environments
+* local OS X install, R 3.4.0
+* ubuntu 12.04 (on travis-ci), R 3.4.0
+* win-builder (devel and release)
+
+## R CMD check results
+
+0 errors | 0 warnings | 0 notes
+
+## Reverse dependencies
+
+There are no reverse dependencies.
