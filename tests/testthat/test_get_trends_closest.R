fcec2528776289f329d356091e71620b99e9c31f
Michael W. Kearney <kearneymw@missouri.edu>
2017-10-24 21:17:08 -0500

    updated tests for get_trends

--- tests/testthat/test_get_trends_closest.R
+++ tests/testthat/test_get_trends_closest.R
@@ -2,11 +2,11 @@ context("trends_available")
 
 test_that("get_trends_closest returns data frame with correct city name", {
   skip_on_cran()
-  
+
   token <- readRDS("twitter_tokens")
-  x <- get_trends_closest(lat = 40.7, long = -74.0, token = token)
-  
-  expect_equal(is.data.frame(x), TRUE)
+  x <- get_trends(lat = 40.7, lng = -74.0, token = token)
+
+  expect_true(is.data.frame(x))
   expect_named(x)
   expect_true(all(c("trend", "woeid") %in% names(x)))
   expect_gt(nrow(x), 5)
