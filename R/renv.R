dc36ec9cf73e85658c9c9277a4b8d582e88054bd
Michael W. Kearney <kearneymw@missouri.edu>
2017-11-14 09:09:54 -0600

    fixed misplaced exclamation point in are_named, fixing issue #134 and issue #135

--- R/renv.R
+++ R/renv.R
@@ -19,7 +19,7 @@ is_named <- function(x) UseMethod("is_named")
 is_named.default <- function(x) !is.null(names(x))
 
 are_named <- function(x) UseMethod("are_named")
-are_named.default <- function(x) !is_named(x) & "" %in% names(x)
+are_named.default <- function(x) is_named(x) & !"" %in% names(x)
 
 readlines <- function(x, ...) {
   con <- file(x)
