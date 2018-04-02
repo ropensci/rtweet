ee13ebc2bf321c0a465145c118ca67bb8b518506
Mike Kearney <kearneymw@missouri.edu>
2018-04-02 14:39:14 -0500

    added more ts_plot tests

--- tests/testthat/test_get_timelines.R
+++ tests/testthat/test_get_timelines.R
@@ -7,11 +7,13 @@ test_that("get_timeline", {
   token <- readRDS("twitter_tokens")
   x <- get_timeline(c("cnnbrk", "cnn"), n = n, token = token)
   xts <- ts_data(x, by = "hours", trim = 1)
+  ts_plot(xts)
   if (requireNamespace("ggplot2", quietly = TRUE) && requireNamespace("dplyr", quietly = TRUE)) {
     p <- ts_plot(dplyr::group_by(x, screen_name, is_retweet))
     expect_true(inherits(p, "ggplot"))
   }
-
+  class(xx) <- c("grouped_df", "tbl_df", "data.frame")
+  ts_plot(xx, "45 mins")
   expect_equal(is.data.frame(x), TRUE)
   expect_named(x)
   expect_true("status_id" %in% names(x))
@@ -19,4 +21,5 @@ test_that("get_timeline", {
   expect_gt(ncol(x), 25)
   expect_true(is.data.frame(users_data(x)))
   expect_true(is.data.frame(xts))
+  unlink("Rplots.pdf")
 })
