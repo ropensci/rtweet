e5652d10b01c2823924709a1c10a11c57c1e539f
Michael W. Kearney <kearneymw@missouri.edu>
2018-06-13 15:54:48 -0500

    improve drop/add and slug-id in `post_list()`, closing issue #210

--- R/post.R
+++ R/post.R
@@ -411,7 +411,10 @@ post_friendship <- function(user,
 
 
 
-post_list_create <- function(name, private = FALSE, description = NULL, token = NULL) {
+post_list_create <- function(name,
+                             description = NULL,
+                             private = FALSE,
+                             token = NULL) {
 
   stopifnot(is.atomic(name), length(name) == 1, is.logical(private))
 
@@ -440,7 +443,9 @@ post_list_create <- function(name, private = FALSE, description = NULL, token = 
 }
 
 
-post_list_destroy <- function(list_id = NULL, slug = NULL, token = NULL) {
+post_list_destroy <- function(list_id = NULL,
+                              slug = NULL,
+                              token = NULL) {
   if (!is.null(list_id)) {
 
     stopifnot(is.atomic(list_id), length(list_id) == 1)
@@ -449,7 +454,7 @@ post_list_destroy <- function(list_id = NULL, slug = NULL, token = NULL) {
   } else if (!is.null(slug)) {
 
     stopifnot(is.atomic(slug), length(slug) == 1)
-    params <- list(slug = slug)
+    params <- list(slug = slug, owner_screen_name = home_user())
 
   } else {
     stop("must supply list_id or slug")
@@ -468,33 +473,118 @@ post_list_destroy <- function(list_id = NULL, slug = NULL, token = NULL) {
   r
 }
 
-post_list_create_all <- function(list_id,
-                                 slug,
-                                 users,
+post_list_create_all <- function(users,
+                                 list_id = NULL,
+                                 slug = NULL,
                                  token = NULL) {
+  ## must id list via numeric ID or slug
+  if (is.null(list_id) && is.null(slug)) {
+    stop("must supply either list_id or slug to identify pre-existing list")
+  }
 
-  stopifnot(is.atomic(list_id), is.atomic(slug),
-    is.atomic(users))
+  ## check and reformat users
+  stopifnot(is.character(users))
+  if (length(users) > 100) {
+    warning("Can only add 100 users at a time. Adding users[1:100]...",
+      call. = FALSE)
+    users <- users[1:100]
+  }
+  users <- paste(users, collapse = ",")
+
+  ## check token
+  token <- check_token(token)
+
+  ## specify API path
+  query <- "lists/members/create_all"
+
+  ## if list id
+  if (!is.null(list_id)) {
+    stopifnot(is.atomic(list_id), length(list_id) == 1)
+    params <- list(
+      list_id = list_id,
+      users = users
+    )
+
+  ## if slug
+  } else {
+    stopifnot(is.atomic(slug), length(slug) == 1)
+    params <- list(
+      slug = slug,
+      owner_screen_name = home_user(),
+      users = users
+    )
+  }
+  ## rename last param
+  names(params)[length(params)] <- .ids_type(users)
+
+  ## build URL
+  url <- make_url(query = query, param = params)
+
+  ## send request
+  r <- TWIT(get = FALSE, url, token)
+
+  ## check status
+  warn_for_twitter_status(r)
+
+  ## return response object
+  r
+}
+
+post_list_destroy_all <- function(users,
+                                  list_id = NULL,
+                                  slug = NULL,
+                                  token = NULL) {
+  ## must id list via numeric ID or slug
+  if (is.null(list_id) && is.null(slug)) {
+    stop("must supply either list_id or slug to identify pre-existing list")
+  }
+
+  ## check and reformat users
+  stopifnot(is.character(users))
+  if (length(users) > 100) {
+    warning("Can only drop 100 users at a time. Dropping users[1:100]...",
+      call. = FALSE)
+    users <- users[1:100]
+  }
 
   users <- paste(users, collapse = ",")
 
+  ## check token
   token <- check_token(token)
 
-  query <- "lists/members/create_all"
+  ## specify API path
+  query <- "lists/members/destroy_all"
 
-  params <- list(
-    list_id = list_id,
-    slug = slug,
-    users = users)
+  ## if list id
+  if (!is.null(list_id)) {
+    stopifnot(is.atomic(list_id), length(list_id) == 1)
+    params <- list(
+      list_id = list_id,
+      users = users
+    )
 
-  names(params)[3] <- .ids_type(users)
+  ## if slug
+  } else {
+    stopifnot(is.atomic(slug), length(slug) == 1)
+    params <- list(
+      slug = slug,
+      owner_screen_name = home_user(),
+      users = users
+    )
+  }
+  ## rename last param
+  names(params)[length(params)] <- .ids_type(users)
 
+  ## build URL
   url <- make_url(query = query, param = params)
 
+  ## send request
   r <- TWIT(get = FALSE, url, token)
 
+  ## check status
   warn_for_twitter_status(r)
 
+  ## return response object
   r
 }
 
@@ -521,28 +611,78 @@ post_list_create_all <- function(list_id,
 #'   "cnnnewsroom", "cnnspecreport", "CNNNewsource",
 #'   "CNNNSdigital", "CNNTonight")
 #'
-#' ## create CNN-accounts list with users and a description
-#' cnn_lst <- post_list(users, "cnn-accounts", description = "Official CNN accounts")
+#' ## create CNN-accounts list with 9 total users
+#' (cnn_lst <- post_list(users,
+#'   "cnn-accounts", description = "Official CNN accounts"))
 #'
+#' ## view list in browser
+#' browseURL(sprintf("https://twitter.com/%s/lists/cnn-accounts",
+#'   rtweet:::home_user()))
+#'
+#' ## search for more CNN users
+#' cnn_users <- search_users("CNN", n = 200)
+#'
+#' ## filter and select more users to add to list
+#' more_users <- cnn_users %>%
+#'   subset(verified & !tolower(screen_name) %in% tolower(users)) %>%
+#'   .$screen_name %>%
+#'   grep("cnn", ., ignore.case = TRUE, value = TRUE)
+#'
+#' ## add more users to list- note: can only add up to 100 at a time
+#' post_list(users = more_users, slug = "cnn-accounts")
+#'
+#' ## view updated list in browser (should be around 100 users)
+#' browseURL(sprintf("https://twitter.com/%s/lists/cnn-accounts",
+#'   rtweet:::home_user()))
+#'
+#' ## select users on list without "cnn" in their name field
+#' drop_users <- cnn_users %>%
+#'   subset(screen_name %in% more_users & !grepl("cnn", name, ignore.case = TRUE)) %>%
+#'   .$screen_name
+#'
+#' ## drop these users from the cnn list
+#' post_list(users = drop_users, slug = "cnn-accounts",
+#'   destroy = TRUE)
+#'
+#' ## view updated list in browser (should be around 100 users)
+#' browseURL(sprintf("https://twitter.com/%s/lists/cnn-accounts",
+#'   rtweet:::home_user()))
+#'
+#' ## delete list entirely
+#' post_list(slug = "cnn-accounts", destroy = TRUE)
 #'
 #' }
 #' @export
-post_list <- function(users, name,
-                      private = FALSE,
+post_list <- function(users = NULL,
+                      name = NULL,
                       description = NULL,
+                      private = FALSE,
                       destroy = FALSE,
                       list_id = NULL,
                       slug = NULL,
                       token = NULL) {
-  if (destroy) {
+  ## destroy list
+  if (destroy && is.null(users)) {
     return(post_list_destroy(list_id, slug, token))
   }
-  r <- post_list_create(name, private, description, token)
-  if (r$status_code != 200) {
-    warning("failed to create list")
-    r
+
+  ## drop users from list
+  if (destroy) {
+    return(post_list_destroy_all(users, list_id, slug, token))
   }
-  tl <- from_js(r)
-  r <- post_list_create_all(tl$id_str, tl$slug, users)
-  r
+
+  ## create list
+  if (is.null(list_id) && is.null(slug)) {
+    r <- post_list_create(name, description, private, token)
+    if (r$status_code != 200) {
+      stop("failed to create list")
+    }
+    ## use returned list ID to add users
+    tl <- from_js(r)
+    list_id <- tl$id_str
+  }
+
+  ## add users to list
+  post_list_create_all(users, list_id, slug, token)
 }
+
