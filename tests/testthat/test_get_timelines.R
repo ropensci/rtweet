7b776d3a780cd540f8152b73318e31e1a3f1b910
Michael W. Kearney <kearneymw@missouri.edu>
2018-06-16 22:08:01 -0500

    rm test line

--- tests/testthat/test_get_timelines.R
+++ tests/testthat/test_get_timelines.R
@@ -20,7 +20,6 @@ test_that("get_timeline", {
   p <- ts_plot(g, "hours", trim = 1)
   expect_true(inherits(p, "ggplot"))
   d <- ts_data(g, "hours", trim = 1)
-  expect_true(is.data.frame(g), TRUE)
   expect_true(is.data.frame(d), TRUE)
   expect_equal(ncol(d), 4)
   unlink("Rplots.pdf")
