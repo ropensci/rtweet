6effd2523dddcafa05b3a81de5772372686c1cd0
Michael W. Kearney <kearneymw@missouri.edu>
2018-06-18 08:46:43 -0500

    add tweet_shot test

---
+++ tests/testthat/test_tweet_shot.R
@@ -0,0 +1,8 @@
+context("system_token")
+
+test_that("tweet_shot", {
+  skip_on_cran()
+  tw <- rtweet::tweet_shot("https://twitter.com/jhollist/status/947082036019388416")
+  expect_equal(class(tw), "magick-image")
+  expect_equal(length(tw), 1L)
+})
