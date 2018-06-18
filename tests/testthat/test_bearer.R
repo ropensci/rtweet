768476a2fecaf1f602f4f9fffd9bc03bdf6f1fc2
Michael W. Kearney <kearneymw@missouri.edu>
2018-06-18 17:18:55 -0500

    Update test_bearer.R

--- tests/testthat/test_bearer.R
+++ tests/testthat/test_bearer.R
@@ -5,6 +5,6 @@ test_that("bearer_token functions", {
 
   token <- readRDS("twitter_tokens")
   x <- search_tweets("lang:en", n = 300, token = bearer_token(token))
-  expect_true(is.dta.frame(x))
+  expect_true(is.data.frame(x))
   expect_gt(nrow(x), 50)
 })
