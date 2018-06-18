24952bfc07a1dc727517b2e1dae8bb1204f355c5
Michael W. Kearney <kearneymw@missouri.edu>
2018-06-18 17:04:17 -0500

    pass token to bearer_token

--- tests/testthat/test_bearer.R
+++ tests/testthat/test_bearer.R
@@ -4,7 +4,7 @@ test_that("bearer_token functions", {
   skip_on_cran()
 
   token <- readRDS("twitter_tokens")
-  x <- search_tweets("lang:en", n = 300, token = bearer_token())
+  x <- search_tweets("lang:en", n = 300, token = bearer_token(token))
   expect_true(is.dta.frame(x))
   expect_gt(nrow(x), 50)
 })
