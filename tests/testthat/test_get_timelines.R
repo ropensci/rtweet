9b80777f94fe3c977f6e107a3893271c926871f2
Michael W. Kearney <kearneymw@missouri.edu>
2018-07-26 20:19:29 -0500

    fix grouped_df test

--- tests/testthat/test_get_timelines.R
+++ tests/testthat/test_get_timelines.R
@@ -17,7 +17,7 @@ test_that("get_timeline", {
   expect_true(inherits(p, "ggplot"))
   if (requireNamespace("ggplot2", quietly = TRUE)) {
     g <- readRDS("g.rds")
-    g <- structure(g, class = c("grouped_df", "data.frame"))
+    #g <- structure(g, class = c("grouped_df", "data.frame"))
     p <- ts_plot(g, "hours", trim = 1)
     expect_true(inherits(p, "ggplot"))
   }
