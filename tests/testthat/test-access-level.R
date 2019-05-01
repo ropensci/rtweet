11920cd646e5df13de93a458ef0ae456841a0258
Michael W. Kearney <kearneymw@missouri.edu>
2019-04-30 23:50:55 -0500

    Update test-access-level.R

--- tests/testthat/test-access-level.R
+++ tests/testthat/test-access-level.R
@@ -3,5 +3,5 @@ context("test-access-level")
 test_that("api_access_level works", {
   token <- readRDS("twitter_tokens")
   a <- api_access_level(token)
-  expect_true(is.character(a), length(a) == 1)
+  expect_true(is.character(a) && length(a) == 1)
 })
