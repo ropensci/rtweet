210a1ea2f1ad08ec731653645675c1fa14f358f4
mkearney <kearneymw@missouri.edu>
2019-12-19 12:22:42 -0600

    Fixes to tests

--- tests/testthat/test-access-level.R
+++ tests/testthat/test-access-level.R
@@ -1,6 +1,19 @@
 context("test-access-level")
 
 test_that("api_access_level works", {
+  if (!file.exists("twiter_tokens")) {
+    token <- rtweet::create_token(
+      "rstats2twitter",
+      consumer_key = rtweet:::decript_key(),
+      consumer_secret = rtweet:::decript_secret(),
+      access_secret = rtweet:::rtweet_find_access_secret(),
+      access_token = rtweet:::rtweet_find_access_key(),
+      set_renv = FALSE
+    )
+    e <- tryCatch(rtweet:::api_access_level(token),
+      error = function(e) NULL)
+    saveRDS(token, "twitter_tokens")
+  }
   token <- readRDS("twitter_tokens")
   a <- api_access_level(token)
   expect_true(is.character(a) && length(a) == 1)
---
+++ tests/testthat/test-zzz.R
@@ -0,0 +1,5 @@
+test_that("destroytoken", {
+  if (file.exists("twitter_tokens")) {
+    unlink("twitter_tokens")
+  }
+})
