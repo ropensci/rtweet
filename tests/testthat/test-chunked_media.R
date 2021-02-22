0b00a54bca7a91c4ecf28b604a64954c7f260a74
Hadley Wickham <h.wickham@gmail.com>
2021-02-22 17:03:07 -0600

    Fix chunked media tests

--- tests/testthat/test-chunked_media.R
+++ tests/testthat/test-chunked_media.R
@@ -6,8 +6,8 @@ test_that("mp4 10MG works", {
   temp <- tempfile(fileext = ".mp4")
   url <-
     "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1280_10MG.mp4"
-  download.file(url, temp, mode = "wb")
-  upload_media_to_twitter(temp, token)
+  download.file(url, temp, mode = "wb", quiet = TRUE)
+  upload_media_to_twitter(temp)
 })
 
 
@@ -19,8 +19,8 @@ test_that("mp4 5MG works", {
   temp <- tempfile(fileext = ".mp4")
   url <-
     "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4"
-  download.file(url, temp, mode = "wb")
-  upload_media_to_twitter(temp, token)
+  download.file(url, temp, mode = "wb", quiet = TRUE)
+  upload_media_to_twitter(temp)
 })
 ## GIF----
 
@@ -31,8 +31,8 @@ test_that("GIF > 5MG works", {
   skip_on_cran()
   temp <- tempfile(fileext = ".gif")
   url <- "https://upload.wikimedia.org/wikipedia/commons/9/96/.Animation_of_Hayabusa2_orbit.gif"
-  download.file(url, temp, mode = "wb")
-  upload_media_to_twitter(temp, token)
+  download.file(url, temp, mode = "wb", quiet = TRUE)
+  upload_media_to_twitter(temp)
 })
 
 ### Sample animated gif (<5Mb)----
@@ -41,8 +41,8 @@ test_that("animated gif < 5MG works", {
   skip_on_cran()
   temp <- tempfile(fileext = ".gif")
   url <- "https://upload.wikimedia.org/wikipedia/commons/d/d0/01_Das_Sandberg-Modell.gif"
-  download.file(url, temp, mode = "wb")
-  upload_media_to_twitter(temp, token)
+  download.file(url, temp, mode = "wb", quiet = TRUE)
+  upload_media_to_twitter(temp)
 })
 
 
@@ -52,8 +52,8 @@ test_that("unanimated gif < 5MG works", {
   skip_on_cran()
   temp <- tempfile(fileext = ".gif")
   url <- "https://file-examples-com.github.io/uploads/2017/10/file_example_PNG_500kB.png"
-  download.file(url, temp, mode = "wb")
-  upload_media_to_twitter(temp, token)
+  download.file(url, temp, mode = "wb", quiet = TRUE)
+  upload_media_to_twitter(temp)
 })
 
 ### Sample unanimated gif (>5Mb) SHOULD ERROR-----
@@ -62,7 +62,7 @@ test_that("unanimated gif >5MG works", {
   skip_on_cran()
   temp <- tempfile(fileext = ".gif")
   url <- "https://sample-videos.com/img/Sample-png-image-5mb.png"
-  download.file(url, temp, mode = "wb")
+  download.file(url, temp, mode = "wb", quiet = TRUE)
   expect_error(upload_media_to_twitter(temp))
 })
 
@@ -72,6 +72,6 @@ test_that("png works", {
   skip_on_cran()
   temp <- tempfile(fileext = ".png")
   url <- "https://file-examples-com.github.io/uploads/2017/10/file_example_PNG_500kB.png"
-  download.file(url, temp, mode = "wb")
+  download.file(url, temp, mode = "wb", quiet = TRUE)
   upload_media_to_twitter(temp)
 })
