3ed01e6312f26672f191371e0f2a352263c18648
dieghernan <diego.hernangomezherrero@gmail.com>
2021-02-28 13:24:18 +0100

    Chunk mp4 + tests (#511)

--- tests/testthat/test-post.R
+++ tests/testthat/test-post.R
@@ -1,12 +1,19 @@
 test_that("upload_media_to_twitter() can handle small file", {
   skip_on_cran()
-  
-  id <- upload_media_to_twitter(test_path("tweet.gif"))
+
+  # gif - unchunk 1MB
+  id <- upload_media_to_twitter(test_path("tweet.gif"), chunk_size = 1024 * 1024)
   expect_type(id, "character")
+
   # doesn't appear to be any way to get info about a media object
   # after it's been uploaded
-  
-  id <- upload_media_to_twitter(test_path("tweet.gif"), chunk_size = 1024)
+
+  # gif - chunk 5KB
+  id <- upload_media_to_twitter(test_path("tweet.gif"), chunk_size = 5 * 1024)
+  expect_type(id, "character")
+
+  # mp4 - test forced chunk / small file w/ high chunk sizes
+  id <- upload_media_to_twitter(test_path("tweet.mp4"), chunk_size = 1024 * 1024)
   expect_type(id, "character")
 })
 
