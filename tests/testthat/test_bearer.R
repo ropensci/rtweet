01b15048958dde5170374d00d0b5606bb358e51e
Hadley Wickham <h.wickham@gmail.com>
2021-03-05 11:10:30 -0600

    Remove old tests

--- tests/testthat/test_bearer.R
+++
@@ -1,8 +0,0 @@
-test_that("bearer_token functions", {
-  skip_on_cran()
-  skip_if_offline()
-
-  x <- search_tweets("lang:en", n = 300, token = bearer_token())
-  expect_true(is.data.frame(x))
-  expect_gt(nrow(x), 50)
-})
