3a6a90f35dcaa347befb8e2531a8c26b656d698d
llrs <lluis.revilla@gmail.com>
2021-03-06 14:11:11 +0100

    Check response is ok

--- tests/testthat/test-post_favorite.R
+++ tests/testthat/test-post_favorite.R
@@ -1,5 +1,5 @@
 test_that("post_favorite works", {
   rt <- search_tweets("#rstats", n = 1)
-  post_favorite(rt$status_id)
-  post_favorite(rt$status_id, destroy = TRUE)
+  expect_equal(httr::status_code(post_favorite(rt$status_id)), 200)
+  expect_equal(httr::status_code(post_favorite(rt$status_id, destroy = TRUE)), 200)
 })
