4a0e3fce8dfacdf426922a6ff06343c25379e0c3
llrs <lluis.revilla@gmail.com>
2021-03-06 16:35:25 +0100

    Two more tests

---
+++ tests/testthat/test-post_message.R
@@ -0,0 +1,6 @@
+test_that("post_message works", {
+  rt <- expect_message(post_message(paste("Testing", Sys.time()), 
+                                    user = "Lluis_Revilla"),
+                       "your DM has been posted!")
+  expect_equal(httr::status_code(rt), 200L)
+})
