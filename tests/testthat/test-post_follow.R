485a4b01ac2d5d20dbfa625be2c746e410eb0d4d
llrs <lluis.revilla@gmail.com>
2021-03-07 12:46:51 +0100

    Remove test that I don't know how should work

--- tests/testthat/test-post_follow.R
+++ tests/testthat/test-post_follow.R
@@ -8,15 +8,6 @@ test_that("post_destroy works", {
   expect_equal(httr::status_code(pf), 200L)
 })
 
-test_that("post_follow works without retweets", {
-  # First we need to follow then we can disable retweets
-  pf <- post_follow("BarackObama") 
-  pf <- post_follow("BarackObama", retweets = FALSE)
-  expect_equal(httr::status_code(pf), 200L)
-  pf <- post_follow("BarackObama", destroy = TRUE)
-  expect_equal(httr::status_code(pf), 200L)
-})
-
 test_that("post_follow works without notifications", {
   pf <- post_follow("BarackObama", notify = FALSE)
   expect_equal(httr::status_code(pf), 200L)
