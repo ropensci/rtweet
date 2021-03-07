1837a2f7761b5dd0f4c7207eb26acb36f201a5b7
llrs <lluis.revilla@gmail.com>
2021-03-07 12:32:32 +0100

    Add more test to specific cases

--- tests/testthat/test-post_follow.R
+++ tests/testthat/test-post_follow.R
@@ -2,7 +2,24 @@ test_that("post_follow works", {
   pf <- post_follow("BarackObama")
   expect_equal(httr::status_code(pf), 200L)
 })
+
 test_that("post_destroy works", {
+  pf <- post_follow("BarackObama", destroy = TRUE)
+  expect_equal(httr::status_code(pf), 200L)
+})
+
+test_that("post_follow works without retweets", {
+  # First we need to follow then we can disable retweets
+  pf <- post_follow("BarackObama") 
+  pf <- post_follow("BarackObama", retweets = FALSE)
+  expect_equal(httr::status_code(pf), 200L)
+  pf <- post_follow("BarackObama", destroy = TRUE)
+  expect_equal(httr::status_code(pf), 200L)
+})
+
+test_that("post_follow works without notifications", {
+  pf <- post_follow("BarackObama", notify = FALSE)
+  expect_equal(httr::status_code(pf), 200L)
   pf <- post_follow("BarackObama", destroy = TRUE)
   expect_equal(httr::status_code(pf), 200L)
 })
