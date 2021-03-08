097e96c9209e27b23a0617499903c5e5583c6380
llrs <lluis.revilla@gmail.com>
2021-03-08 18:39:41 +0100

    Fix indentation

--- tests/testthat/test-network_data.R
+++ tests/testthat/test-network_data.R
@@ -1,10 +1,9 @@
 test_that("network_data works", {
-    rstats <- search_tweets("#rstats", n = 20)
-
-    ## create from-to data frame representing retweet/mention/reply connections
-    rstats_net <- network_data(rstats, "retweet,mention,reply")
-    expect_s3_class(rstats_net, "data.frame")
-    expect_equal(colnames(rstats_net), c("from", "to", "type"))
+   rstats <- search_tweets("#rstats", n = 20)
+   ## create from-to data frame representing retweet/mention/reply connections
+   rstats_net <- network_data(rstats, "retweet,mention,reply")
+   expect_s3_class(rstats_net, "data.frame")
+  expect_equal(colnames(rstats_net), c("from", "to", "type"))
 })
 
 
