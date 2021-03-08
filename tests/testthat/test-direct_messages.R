832fa617282c92fd63e6e48e13aa84349f35e943
Lluís <llrs@users.noreply.github.com>
2021-03-08 14:47:13 +0100

    Merge old functions tests for direct_messages
    Co-authored-by: Hadley Wickham <h.wickham@gmail.com>

--- tests/testthat/test-direct_messages.R
+++ tests/testthat/test-direct_messages.R
@@ -4,10 +4,7 @@ test_that("direct_messages works", {
   expect_length(dm, 2L)
 })
 
-test_that("direct_messages_received works", {
+test_that("old functions give informative errors", {
   expect_error(direct_messages_received(), "no longer exists")
-})
-
-test_that("direct_messages_sent works", {
   expect_error(direct_messages_sent(), "no longer exists")
 })
