95640a7ac5ae507f4bd71901b044b56f66cf50ea
Lluís <llrs@users.noreply.github.com>
2021-03-08 18:41:13 +0100

    Change style
    Co-authored-by: Hadley Wickham <h.wickham@gmail.com>

--- tests/testthat/test-post_message.R
+++ tests/testthat/test-post_message.R
@@ -1,6 +1,7 @@
 test_that("post_message works", {
-  rt <- expect_message(post_message(paste("Testing", Sys.time()), 
-                                    user = "Lluis_Revilla"),
-                       "your DM has been posted!")
+  rt <- expect_message(
+    post_message(paste("Testing", Sys.time()), user = "Lluis_Revilla"),
+    "your DM has been posted!"
+  )
   expect_equal(httr::status_code(rt), 200L)
 })
