c7d6b8d9b45478e0deed12ee268a4ac157242e9d
llrs <lluis.revilla@gmail.com>
2021-03-08 18:40:40 +0100

    Change to non default notify

--- tests/testthat/test-post_follow.R
+++ tests/testthat/test-post_follow.R
@@ -8,8 +8,8 @@ test_that("post_destroy works", {
   expect_equal(httr::status_code(pf), 200L)
 })
 
-test_that("post_follow works without notifications", {
-  pf <- post_follow("BarackObama", notify = FALSE)
+test_that("post_follow works with notifications", {
+  pf <- post_follow("BarackObama", notify = TRUE)
   expect_equal(httr::status_code(pf), 200L)
   pf <- post_follow("BarackObama", destroy = TRUE)
   expect_equal(httr::status_code(pf), 200L)
