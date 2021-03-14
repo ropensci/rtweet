2497aad0b7716bd4086a27ebab1ffd7fc3d99589
llrs <lluis.revilla@gmail.com>
2021-03-14 17:43:07 +0100

    Test to close #467

--- tests/testthat/test-post-user.R
+++ tests/testthat/test-post-user.R
@@ -14,3 +14,12 @@ test_that("post_follow works with notifications", {
   pf <- post_follow("BarackObama", destroy = TRUE)
   expect_equal(httr::status_code(pf), 200L)
 })
+
+
+test_that("Muting  #467", {
+  pf <- post_follow("hlynur", destroy = TRUE, mute = TRUE, notify = TRUE) 
+  expect_equal(httr::status_code(pf), 200L)
+  # Unmutting without following
+  pf <- post_follow("hlynur", destroy = TRUE, mute = FALSE, notify = TRUE)
+  expect_equal(httr::status_code(pf), 200L)
+})
