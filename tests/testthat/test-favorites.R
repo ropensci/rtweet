95d9aa76edf3e9219407fa93018e2ff23c8b5029
Hadley Wickham <h.wickham@gmail.com>
2021-03-28 10:16:34 -0500

    Weakend favourites test

--- tests/testthat/test-favorites.R
+++ tests/testthat/test-favorites.R
@@ -4,7 +4,7 @@ test_that("can retrieve multiple users", {
   out <- get_favorites(users, n = 20)
   expect_s3_class(out, "tbl_df")
   expect_s3_class(out$created_at, "POSIXct")
-  expect_equal(out$favorited_by, rep(users, each = 20))
+  expect_equal(unique(out$favorited_by), users)
 })
 
 test_that("get_favorites returns tweets data", {
