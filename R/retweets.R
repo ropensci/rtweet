3984196792b5ffa517c0859c4ceef537539fcefb
Hadley Wickham <h.wickham@gmail.com>
2021-04-02 09:47:51 -0500

    get_rtweeters doesn't need pagination
    Since it can return at most 100 users.

--- R/retweets.R
+++ R/retweets.R
@@ -1,18 +1,16 @@
-#' Get the most recent retweets of a specific Twitter status
+#' Get the most recent retweets/retweeters
 #'
-#' Returns a collection of the 100 most recent retweets of a given
-#' status.  NOTE: Twitter's API is currently limited to 100 or fewer
-#' retweeters.
+#' `get_retweets()` returns the 100 most recent retweets of a tweet;
+#' `get_retweeters()` retursn the 100 most recent users who retweeted them.
 #'
 #' @inheritParams lookup_users
-#' @param status_id required The numerical ID of the desired status.
-#' @param n optional Specifies the number of records to retrieve.
-#'   Must be less than or equal to 100.
+#' @param status_id Tweet/status id.
+#' @param n Number of results to retrieve. Must be <= 100.
 #' @param ... Other arguments used as parameters in the query sent to
 #'   Twitter's rest API, for example, `trim_user = TRUE`.
-#' @return Tweets data of the most recent retweets of a given status
+#' @return Tweets data of the most recent retweets/retweeters of a given status
 #' @export
-#' @family retweets
+#' @references <https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/get-statuses-retweets-id>
 get_retweets <- function(status_id, n = 100, parse = TRUE, token = NULL, ...) {
   stopifnot(is.character(status_id), length(status_id) == 1L)
   
@@ -30,45 +28,19 @@ get_retweets <- function(status_id, n = 100, parse = TRUE, token = NULL, ...) {
   r
 }
 
-
-#' Get user IDs of users who retweeted a given status.
-#'
-#' Returns user IDs of users who retweeted a given status. At the
-#' current time, this function is limited in returning a maximum of
-#' 100 users for a given status.
-#'
-#' @inheritParams lookup_users
-#' @param status_id required The status ID of the desired status.
-#' @param n Specifies the number of records to retrieve.  Best if
-#'   intervals of 100.
-#' @return data
-#' @family retweets
 #' @export
 #' @references <https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/get-statuses-retweeters-ids>
-get_retweeters <- function(status_id,
-                           n = 100,
-                           parse = TRUE,
-                           token = NULL) {
-  
-  
+#' @rdname get_retweets
+get_retweeters <- function(status_id, n = 100, parse = TRUE, token = NULL) {
   params <- list(
     id = status_id,
+    count = n,
     stringify_ids = TRUE
   )
-
-  r <- TWIT_paginate_cursor(token, "/1.1/statuses/retweeters/ids", 
-    params = params,
-    page_size = 100
-  )
+  r <- TWIT_get(token, "/1.1/statuses/retweeters/ids", params = params)
   
   if (parse) {
-    r <- parse_retweeters(r)
+    r <- tibble::tibble(user_id = r$ids)
   }
   r
-}
-
-
-parse_retweeters <- function(x) {
-  ids <- lapply(x, function(x) x$ids)
-  tibble::tibble(user_id = unlist(ids))
 }
