acfe90d9c211f6bdf4669c69ba54c295088df54e
Hadley Wickham <h.wickham@gmail.com>
2021-04-05 10:41:56 -0500

    Polish search_users() docs

--- R/search_users.R
+++ R/search_users.R
@@ -1,25 +1,12 @@
-#' Get users data on accounts identified via search query.
+#' Search for users
 #'
-#' Returns data for up to 1,000 users matched by user provided search
-#' query.
+#' Search for Twitter users. The Twitter API limits the results to at most 
+#' 1,000 users.
 #'
-#' @inheritParams lookup_users
-#' @param q Query to be searched, used in filtering relevant tweets to
-#'   return from Twitter's REST API. Should be a character string not
-#'   to exceed 500 characters maximum. Spaces are assumed to function
-#'   like boolean "AND" operators. To search for tweets including one
-#'   of multiple possible terms, separate search terms with spaces and
-#'   the word "OR". For example, the search `query =
-#'   "data science"` searches for tweets using both "data" and
-#'   "science" though the words can appear anywhere and in any order
-#'   in the tweet. However, when OR is added between search terms,
-#'   `query = "data OR science"`, Twitter's REST API should
-#'   return any tweet that includes either "data" or "science"
-#'   appearing in the tweets. At this time, Twitter's users/search API
-#'   does not allow complex searches or queries targeting exact
-#'   phrases as is allowed by `search_tweets`.
-#' @param n Number of users to return (at most 1,000).
-#' @param verbose If `TRUE`, will display a progress bar while downloading.
+#' @inheritParams TWIT_paginate_max_id
+#' @param q As string providing the search query. Try searching by interest, 
+#'   full name, company name, or location. Exact match searches are not 
+#'   supported.
 #' @examples
 #'
 #' \dontrun{
@@ -30,7 +17,7 @@
 #' tweets_data(users)
 #' }
 #'
-#' @return Data frame of users returned by query.
+#' @return Data frame with one row for each matching user.
 #' @family users
 #' @export
 #' @references <https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/follow-search-get-users/api-reference/get-users-search>
@@ -41,7 +28,7 @@ search_users <- function(q, n = 100,
   
   stopifnot(is_n(n), is.atomic(q))
   if (n > 1000) {
-    abort("`n` must be <= 1,000")
+    abort("`n` must be <= 1,000 (the maximum allowed by Twitter)")
   }
 
   pages <- ceiling(n / 20)
