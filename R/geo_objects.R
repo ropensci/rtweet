6d5f866cb9e712583604f662e2dead098a9c6d42
llrs <lluis.revilla@gmail.com>
2021-05-04 22:31:31 +0200

    Changing name to match the API

--- R/geo_objects.R
+++ R/geo_objects.R
@@ -3,23 +3,25 @@
 
 bounding_box <- function(x) {
   if(is.null(x)) {
-    data.frame(long = NA, lat = NA, type = NA)
+    as_tbl(data.frame(long = NA, lat = NA, type = NA))
   }
   
   m <- x$coordinates[1, , ]
   colnames(m) <- c("long", "lat")
   df <- as.data.frame(m)
   df$type <- x$type
-  df
+  as_tbl(df)
 }
 
 # <https://developer.twitter.com/en/docs/twitter-api/v1/data-dictionary/object-model/geo#coordinates>
-geo <- function(x) {
-  if (is.null(x)) {
-    return(data.frame(long = NA, lat = NA, type = NA))
+coordinates <- function(x) {
+  if (is.null(x) || is.na(x)) {
+    return(as_tbl(data.frame(long = NA, lat = NA, type = NA)))
   }
-  
-  data.frame(long = x$coordinates[[1]], lat = x$coordinates[[2]], type = x$type)
+  if (has_name_children(x, "coordinates", "coordinates")) {
+    return(as_tbl(data.frame(long = x$coordinates[[1]], lat = x$coordinates[[2]], type = x$type)))
+  }
+  as_tbl(data.frame(long = x[[1]], lat = x[[2]], type = x$type))
 }
 
 # <https://developer.twitter.com/en/docs/twitter-api/v1/data-dictionary/object-model/geo#place>
@@ -27,14 +29,14 @@ place <- function(x) {
   if (is.null(x)) {
     df <- data.frame(geo = I(list(NA)), coordinates = I(list(NA)),
                              place = I(list(NA)))
-    return(df)
+    return(as_tbl(df))
   }
   l <- simplify2array(x$place[!names(x$place) %in% "bounding_box"])
   l[lengths(l) == 0] <- NA
   place <- as.data.frame(l)
   place$bounding_box <- list(bounding_box(x$place$bounding_box))
   
-  tibble::tibble(geo = geo(x$geo),
-            coordinates = geo(x$coordinates),
+  tibble::tibble(geo = coordinates(x$geo),
+            coordinates = coordinates(x$coordinates),
             place = place)
 }
