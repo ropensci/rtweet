4c6de7769d6c479a998608b564ac2a38e58b05c7
llrs <lluis.revilla@gmail.com>
2021-05-25 20:44:00 +0200

    Specific checks on different type of tweets

--- tests/testthat/test-graph-network.R
+++ tests/testthat/test-graph-network.R
@@ -16,12 +16,11 @@ test_that("graphing functions work", {
 test_that("network_data works", {
    rstats <- search_tweets("#rstats", n = 20)
    ## create from-to data frame representing retweet/mention/reply connections
-   rstats_net <- network_data(rstats, "retweet,mention,reply")
+   rstats_net <- network_data(rstats, c("retweet","mention","reply"))
    expect_s3_class(rstats_net, "data.frame")
   expect_equal(colnames(rstats_net), c("from", "to", "type"))
 })
 
-lu <- lookup_tweets("1390403676057980928")
 test_that("network_graph works", {
   rstats <- search_tweets("#rstats", n = 20)
   
@@ -29,3 +28,28 @@ test_that("network_graph works", {
   rstats_net <- network_graph(rstats)
   expect_s3_class(rstats_net, "igraph")
 })
+
+# Status (no retweet, no reply no quote)
+test_that("network_data on status", {
+  lu <- lookup_tweets("1333789433288540160")
+  nd <- network_data(lu)  
+})
+
+# Reply (no quote no retweet)
+test_that("network_data on reply", {
+  lu <- lookup_tweets("1333789435482161153")
+  nd <- network_data(lu)  
+})
+
+# Retweet with other tweet embedded quoting 
+test_that("network_data on retweet quotting", {
+  lu <- lookup_tweets("1390610121743556609")
+  nd <- network_data(lu)  
+})
+
+# Retweet without adding anything new
+test_that("network_data on retweet", {
+  lu <- lookup_tweets("1390785143615467524")
+  nd <- network_data(lu)  
+})
+
