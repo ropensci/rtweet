71c0d3b21ed8b7d7a1b688e9b57db852f4b0688a
Alex Hayes <alexpghayes@gmail.com>
2021-07-14 20:30:54 -0500

    Fix parsing failure in lookup_users()

--- R/tweets_and_users.R
+++ R/tweets_and_users.R
@@ -26,8 +26,11 @@ users_with_tweets <- function(x) {
   tweets_raw <- lapply(x, function(x) x[["status"]])
   tweets_tbl <- lapply(tweets_raw, tweets_to_tbl_)
   tweets <- do.call("rbind", tweets_tbl)
-  tweets$user_id <- users$user_id
-  tweets$screen_name <- users$screen_name
+  
+  if (NROW(tweets) > 0) {
+    tweets$user_id <- users$user_id
+    tweets$screen_name <- users$screen_name
+  }
   
   structure(users, tweets = tweets)
 }
