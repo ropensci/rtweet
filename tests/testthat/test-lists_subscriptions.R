e9477a2f3cc7628c47580dccd6a34a897ce05c82
Lluís <lluis.revilla@gmail.com>
2022-03-19 13:12:01 +0100

    More coverage of API calls

--- tests/testthat/test-lists_subscriptions.R
+++ tests/testthat/test-lists_subscriptions.R
@@ -1,6 +1,8 @@
 test_that("lists_subscriptions returns lists data frame", {
   skip("misteriously doesn't work")
-  x <- lists_subscriptions("kearneymw")
+  vcr::use_cassette("lists_subscriptions1", {
+    x <- lists_subscriptions("kearneymw")
+  })
 
   expect_true(is.data.frame(x))
   expect_true("list_id" %in% names(x))
--- tests/testthat/test-mentions.R
+++ tests/testthat/test-mentions.R
@@ -1,5 +1,7 @@
 test_that("mentions returns tweets data", {
-  suppressMessages(x <- get_mentions())
+  vcr::use_cassette("mentions", {
+    suppressMessages(x <- get_mentions())
+  })
 
   expect_s3_class(x, "data.frame")
   expect_gt(nrow(x), 0)
