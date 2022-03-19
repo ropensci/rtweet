0833d7e243f565cc6c88fcec4f465fd1ed2f790e
Lluís <lluis.revilla@gmail.com>
2022-03-19 13:15:33 +0100

    Testing list_subscription should work now

--- tests/testthat/test-lists_subscriptions.R
+++ tests/testthat/test-lists_subscriptions.R
@@ -1,8 +1,7 @@
 test_that("lists_subscriptions returns lists data frame", {
-  skip("misteriously doesn't work")
   x <- lists_subscriptions("kearneymw")
 
   expect_true(is.data.frame(x))
   expect_true("list_id" %in% names(x))
-  expect_gt(nrow(x), 1)
+  expect_true(nrow(x) >= 1)
 })
