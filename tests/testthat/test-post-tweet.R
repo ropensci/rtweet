0d5839fb08b79bd7491c48073d2a1731f34a311e
Lluís <lluis.revilla@gmail.com>
2022-03-19 16:36:25 +0100

    Protect more API calls

--- tests/testthat/test-post-tweet.R
+++ tests/testthat/test-post-tweet.R
@@ -1,34 +1,47 @@
 test_that("upload_media_to_twitter() can handle small file", {
   # gif - unchunk 1MB
-  id <- upload_media_to_twitter(test_path("tweet.gif"), chunk_size = 1024 * 1024)
+  vcr::use_cassette("upload_media_to_twitter1", {
+    id <- upload_media_to_twitter(test_path("tweet.gif"), chunk_size = 1024 * 1024)
+  })
   expect_type(id, "character")
-
+  
   # doesn't appear to be any way to get info about a media object
   # after it's been uploaded
-
+  
   # gif - chunk 5KB
-  id <- upload_media_to_twitter(test_path("tweet.gif"), chunk_size = 5 * 1024)
+  vcr::use_cassette("upload_media_to_twitter2", {
+    id <- upload_media_to_twitter(test_path("tweet.gif"), chunk_size = 5 * 1024)
+  })
   expect_type(id, "character")
-
+  
   # mp4 - test forced chunk / small file w/ high chunk sizes
-  id <- upload_media_to_twitter(test_path("tweet.mp4"), chunk_size = 1024 * 1024)
+  vcr::use_cassette("upload_media_to_twitter3", {
+    id <- upload_media_to_twitter(test_path("tweet.mp4"), chunk_size = 1024 * 1024)
+  })
   expect_type(id, "character")
 })
 
 
 test_that("can set alt text", {
-  id <- upload_media_to_twitter(test_path("tweet.gif"), alt_text = "A bird tweeting")
+  vcr::use_cassette("upload_media_to_twitter4", {
+    id <- upload_media_to_twitter(test_path("tweet.gif"), alt_text = "A bird tweeting")
+  })
   expect_type(id, "character")
 })
 
 
 test_that("post_tweet works", {
   msg <- paste("test", Sys.time()) # To avoid having duplicated status
-  expect_message(pt <- post_tweet(msg), "Your tweet has been posted!")
+  
+  vcr::use_cassette("post_tweet1", {
+    expect_message(pt <- post_tweet(msg), "Your tweet has been posted!")
+  })
   #pt = post tweet
   cpt <- httr::content(pt) 
   expect_equal(httr::status_code(pt), 200L)
-  expect_message(dt <-post_destroy(cpt$id_str), "Your tweet has been deleted!")
+  vcr::use_cassette("post_tweet2", {
+    expect_message(dt <-post_destroy(cpt$id_str), "Your tweet has been deleted!")
+  })
   # dt = destroy tweet
   expect_equal(httr::status_code(dt), 200L)
 })
@@ -36,27 +49,39 @@ test_that("post_tweet works", {
 test_that("post_tweet geolocated works", {
   # Test geolocated tweet
   msg <- paste("test geolocated", Sys.time()) # To avoid having duplicated status
-  expect_message(pt <- post_tweet(msg, lat = -36.811784, long = 174.792657), "Your tweet has been posted!")
+  vcr::use_cassette("post_tweet3", {
+    expect_message(pt <- post_tweet(msg, lat = -36.811784, long = 174.792657), 
+                   "Your tweet has been posted!")
+  })
   cpt <- httr::content(pt)
-  expect_message(dt <- post_destroy(cpt$id_str), "Your tweet has been deleted!")
+  vcr::use_cassette("post_tweet4", {
+    expect_message(dt <- post_destroy(cpt$id_str), "Your tweet has been deleted!")
+  })
   expect_equal(httr::status_code(dt), 200L)
-
+  
   # Test display_coordinates param
   msg <- paste("test geolocated", Sys.time()) # To avoid having duplicated status
-  expect_message(pt <- post_tweet(msg, lat = -36.811784, long = 174.792657, display_coordinates = TRUE), "Your tweet has been posted!")
+  vcr::use_cassette("post_tweet5", {
+    expect_message(pt <- post_tweet(msg, lat = -36.811784, long = 174.792657,
+                                    display_coordinates = TRUE), 
+                   "Your tweet has been posted!")
+  })
   cpt <- httr::content(pt)
-  expect_message(post_destroy(cpt$id_str), "Your tweet has been deleted!")
+  
+  vcr::use_cassette("post_tweet6", {
+    expect_message(post_destroy(cpt$id_str), "Your tweet has been deleted!")
+  })
   expect_equal(httr::status_code(dt), 200L)
 })
 
 test_that("Check geo-related inputs for post_tweet", {
   # All these post_tweets fail
-  expect_snapshot(error = TRUE, {
-    msg <- paste("test geolocated error", Sys.time()) # To avoid having duplicated status
-    post_tweet(msg, lat = "x", long = 0) 
-    post_tweet(msg, lat = 0, long = "x")
-    post_tweet(msg, lat = 91, long = 0)
-    post_tweet(msg, lat = 0, long = 181)
-    post_tweet(msg, lat = 0, long = 0, display_coordinates = "error")
+    expect_snapshot(error = TRUE, {
+      msg <- paste("test geolocated error", Sys.time()) # To avoid having duplicated status
+      post_tweet(msg, lat = "x", long = 0) 
+      post_tweet(msg, lat = 0, long = "x")
+      post_tweet(msg, lat = 91, long = 0)
+      post_tweet(msg, lat = 0, long = 181)
+      post_tweet(msg, lat = 0, long = 0, display_coordinates = "error")
   })
 })
