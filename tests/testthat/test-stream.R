ba16a31049ffed22d2c67ebb0563512a3b54136a
Lluís <lluis.revilla@gmail.com>
2022-03-26 19:13:26 +0100

    Skip tests sometimes

--- tests/testthat/test-stream.R
+++ tests/testthat/test-stream.R
@@ -1,5 +1,9 @@
 test_that("stream_tweets returns tweets data", {
   skip_if_offline()
+  # Randomly skip test to avoid unofficial 420 calm down response from twitter
+  if (sample(c(TRUE, FALSE, FALSE, FALSE), 1)) {
+    skip_on_ci()
+  }
   path <- tempfile()
   x1 <- stream_tweets(timeout = 1, file_name = path, verbose = FALSE)
   expect_s3_class(x1, "data.frame")
