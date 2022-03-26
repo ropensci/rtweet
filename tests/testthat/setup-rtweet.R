57c26aeb03d5e56340711243945605ff90642703
Lluís <lluis.revilla@gmail.com>
2022-03-26 13:17:02 +0100

    Remove message from vcr cassettes

--- tests/testthat/setup-rtweet.R
+++ tests/testthat/setup-rtweet.R
@@ -3,6 +3,7 @@ invisible(vcr::vcr_configure(
   dir = vcr::vcr_test_path("fixtures"),
   filter_request_headers = list(Authorization = "My oauth token is safe",
                                 GOOGLE_KEY = "My google key is safe.",
-                                GOOGLE_MAPS_KEY = "Your google key is safe.")
+                                GOOGLE_MAPS_KEY = "My google key is safe."),
+  filter_query_parameters = c(status = "status message")
 ))
 vcr::check_cassette_names()
