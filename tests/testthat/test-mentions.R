053ccedfe48de9cdea3e7ec071dffbcccddebbad
Lluís <lluis.revilla@gmail.com>
2022-04-28 21:24:42 +0200

    Increment version number to 0.7.0.9022

--- tests/testthat/test-mentions.R
+++ tests/testthat/test-mentions.R
@@ -7,3 +7,17 @@ test_that("mentions returns tweets data", {
   expect_s3_class(x, "data.frame")
   expect_gt(nrow(x), 0)
 })
+
+vcr::use_cassette("mentions2", {
+  test_that("mentions returns tweets format", {
+    
+    tw <- get_mentions()
+    
+    expect_s3_class(tw, "data.frame")
+    expect_gt(nrow(tw), 0)
+    expect_s3_class(users_data(tw), "data.frame")
+    expect_true("id" %in% colnames(tw))
+    
+    expect_error(get_mentions(since_id = tw), NA)
+  })
+})
