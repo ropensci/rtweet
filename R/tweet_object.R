3daab2f491e79d957a1464c3d4bf91f3da6cbef9
Lluís <lluis.revilla@gmail.com>
2022-05-07 12:50:14 +0200

    Simplify the processing of entities
    This might make things slightly faster

--- R/tweet_object.R
+++ R/tweet_object.R
@@ -127,15 +127,14 @@ tweet <- function(x) {
 }
 
 parse_entities2 <- function(y) {
-  l <- vector("list", NROW(y))
-  for (col in seq_len(NCOL(y))) {
+  m <- matrix(nrow = NROW(y), ncol = NCOL(y), dimnames = list(NULL, colnames(y)))
+  df <- as.data.frame(m)
+  for (col in colnames(y)) {
     # Look for the function of said object and save it. 
-    fun <- match.fun(colnames(y)[col])
-    l[[col]] <- lapply(y[[col]], fun)
+    fun <- match.fun(col)
+    df[[col]] <- lapply(y[[col]], fun)
   }
-  # Split and join
-  ll <- transpose_list(l)
-  lapply(ll, `names<-`, value = colnames(y))
+  df
 }
 
 # From https://stackoverflow.com/a/54970694/2886003
