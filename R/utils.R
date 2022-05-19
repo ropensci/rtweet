b6574bf6ef8a5744a81c2b2e9299a9a2724b281e
Lluís <lluis.revilla@gmail.com>
2022-05-20 00:03:01 +0200

    Precommit vignettes should work on ropensci universe
    Dealing with #609 again

--- R/utils.R
+++ R/utils.R
@@ -65,18 +65,15 @@ is_dev_mode <- function() {
   exists(".__DEVTOOLS__", .getNamespace("rtweet"))
 }
 
-is_pkgdown <- function() {
-  identical(Sys.getenv("IN_PKGDOWN"), "true")
-}
-
-is_cran <- function() {
-  identical(Sys.getenv("NOT_CRAN"), "true")
-}
-
 is_rcmd_check <- function() {
   identical(Sys.getenv("RTESTS"), "true")  
 }
 
 is_developing <- function() {
-  is_testing() || (is_dev_mode() %||% is_rcmd_check()) || !is_cran() || is_pkgdown()
+  is_testing() || (is_dev_mode() %||% is_rcmd_check())
+}
+
+
+release_bullets <- function() {
+  c("Run vignette/precompute.R and move images")
 }
