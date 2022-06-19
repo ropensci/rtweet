42157da00cdbf9c6543e5894c3e33152ebe84e14
Lluís <lluis.revilla@gmail.com>
2022-06-20 01:03:29 +0200

    Use the right api

--- R/profile.R
+++ R/profile.R
@@ -8,9 +8,8 @@ profile_banner <- function(banner_file, token = NULL) {
   if (missing(banner_file)) {
     r <- TWIT_get(token, "/1.1/users/profile_banner", list(screen_name = "twitterapi"))
   } else {
-    banner_uri <- base64enc::base64encode(banner_file, linewidth = NA)
-    r <- TWIT_upload(token,  "/1.1/account/update_profile_banner", 
-                   list(image = banner_uri))
+    banner_uri <- base64enc::base64encode(banner_file)
+    r <- TWIT_post(token,  "/1.1/account/update_profile_banner", params = list(banner = banner_uri))
     message("your profile banner image has been updated!")
   }
   r
