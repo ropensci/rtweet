455336563acecfeeb011d9eb5ef9db63389a21c4
Lluís <lluis.revilla@gmail.com>
2022-06-19 10:04:55 +0200

    Fix tests

--- tests/testthat/test-premium.R
+++ tests/testthat/test-premium.R
@@ -1,5 +1,7 @@
 test_that("search_fullarchive works", {
+  skip_on_ci()
   skip_on_cran()
+  skip_on_covr()
   df <- search_fullarchive( q = '#covid place:UK OR place:GB OR place:"United Kindom"',
                             n = 100, env_name = 'fullArchive',
                             fromDate = "201810010000",
@@ -8,7 +10,9 @@ test_that("search_fullarchive works", {
 })
 
 test_that("search_fullarchive queries bigger than page size work", {
+  skip_on_ci()
   skip_on_cran()
+  skip_on_covr()
   df <- search_fullarchive( q = '#covid place:UK OR place:GB OR place:"United Kindom"',
                             n = 200, env_name = 'fullArchive',
                             fromDate = "201810010000",
