92ca2972f5a5474fe16b72a2f82f19f14ddaeddc
Lluís <lluis.revilla@gmail.com>
2022-06-28 00:33:07 +0200

    Skip premium test

--- tests/testthat/test-premium.R
+++ tests/testthat/test-premium.R
@@ -2,6 +2,7 @@ test_that("search_fullarchive works", {
   skip_on_ci()
   skip_on_cran()
   skip_on_covr()
+  skip("requires manual testing")
   df <- search_fullarchive( q = '#covid place:UK OR place:GB OR place:"United Kindom"',
                             n = 20, env_name = 'fullArchive',
                             fromDate = "201810010000",
@@ -13,6 +14,7 @@ test_that("search_fullarchive queries bigger than page size work", {
   skip_on_ci()
   skip_on_cran()
   skip_on_covr()
+  skip("requires manual testing")
   df <- search_fullarchive( q = '#covid place:UK OR place:GB OR place:"United Kindom"',
                             n = 20, env_name = 'fullArchive',
                             fromDate = "201810010000",
