88b2f980e8917d245b5dcb4ff08977069917080d
Lluís <lluis.revilla@gmail.com>
2022-06-27 23:30:00 +0200

    Reduce the amount of tweets retrieved.

--- tests/testthat/test-premium.R
+++ tests/testthat/test-premium.R
@@ -3,10 +3,10 @@ test_that("search_fullarchive works", {
   skip_on_cran()
   skip_on_covr()
   df <- search_fullarchive( q = '#covid place:UK OR place:GB OR place:"United Kindom"',
-                            n = 100, env_name = 'fullArchive',
+                            n = 20, env_name = 'fullArchive',
                             fromDate = "201810010000",
                             token = auth_as("bearer_testing_app"))
-  expect_equal(nrow(df), 100)
+  expect_equal(nrow(df), 20)
 })
 
 test_that("search_fullarchive queries bigger than page size work", {
@@ -14,8 +14,8 @@ test_that("search_fullarchive queries bigger than page size work", {
   skip_on_cran()
   skip_on_covr()
   df <- search_fullarchive( q = '#covid place:UK OR place:GB OR place:"United Kindom"',
-                            n = 200, env_name = 'fullArchive',
+                            n = 20, env_name = 'fullArchive',
                             fromDate = "201810010000",
                             token = auth_as("bearer_testing_app"))
-  expect_equal(nrow(df), 200)
+  expect_equal(nrow(df), 20)
 })
