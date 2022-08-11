b8dfc4e3b683a59f4d2aa6ff821f45091cac2292
Benjamin Soltoff <ben@bensoltoff.com>
2022-08-11 09:10:01 -0400

    Add tests for ts_plot() and ts_data() to ensure they work with grouped data frames

--- tests/testthat/test-ts_plot.R
+++ tests/testthat/test-ts_plot.R
@@ -1,5 +1,5 @@
 test_that("ts_plot works", {
-  
+
   vcr::use_cassette("ts_plot1", {
     rt <- search_tweets("rstats", n = 100)
   })
@@ -8,9 +8,26 @@ test_that("ts_plot works", {
 
 
 test_that("ts_data works", {
-  
+
   vcr::use_cassette("ts_plot2", {
     rt <- search_tweets("rstats", n = 100)
   })
   expect_error(ts_data(rt), NA)
 })
+
+test_that("ts_plot grouped works", {
+
+  vcr::use_cassette("ts_plot3", {
+    rt <- search_tweets("rstats", n = 100)
+  })
+  expect_error(ts_plot(dplyr::group_by(rt, screen_name)), NA)
+})
+
+test_that("ts_data grouped works", {
+
+  vcr::use_cassette("ts_plot4", {
+    rt <- search_tweets("rstats", n = 100)
+  })
+  expect_error(ts_data(dplyr::group_by(rt, screen_name)), NA)
+})
+
