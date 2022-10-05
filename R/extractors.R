3e9906bbfd76f56ce3cbaa8dd8a09b6eb3e6c5c6
Lluís <lluis.revilla@gmail.com>
2022-10-06 00:48:43 +0200

    Make clear that users_data & tweets_data don't call the API

--- R/extractors.R
+++ R/extractors.R
@@ -1,9 +1,9 @@
 #' Get tweets from users, or users from tweets
-#' 
+#'
 #' Twitter API endpoints that return tweets also return data about the users who
-#' tweeted, and most endpoints that return users also return their last tweet. 
-#' Showing these additional columns would clutter the default display, so 
-#' rtweet instead stores in special attributes and allows you to retrieve them 
+#' tweeted, and most endpoints that return users also return their last tweet.
+#' Showing these additional columns would clutter the default display, so
+#' rtweet instead stores in special attributes and allows you to show them
 #' with the `user_data()` and `tweets_data()` helpers.
 #'
 #' @examples
@@ -11,14 +11,16 @@
 #' # find users from tweets
 #' tweets <- search_tweets("r")
 #' users_data(tweets)
-#' 
+#' full_search <- cbind(tweets, users_data(tweets))
+#'
 #' # from tweets from users
 #' users <- search_users("r")
 #' tweets_data(users)
+#' full_users <- cbind(users, tweets_data(users))
 #' }
-#' @return `user_data()` returns a data frame of users; `tweets_data()` 
+#' @return `user_data()` returns a data frame of users; `tweets_data()`
 #'   returns a data frame of tweets.
-#' @param tweets A data frame of tweets. 
+#' @param tweets A data frame of tweets.
 #' @export
 users_data <- function(tweets) {
   users <- attr(tweets, "users", exact = TRUE)
