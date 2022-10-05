c6b661aa06373ed33c327a561e65acc86074ba6b
Lluís <lluis.revilla@gmail.com>
2022-10-06 00:50:03 +0200

    add tests for clean_tweets and fix bugs

--- R/clean_tweets.R
+++ R/clean_tweets.R
@@ -1,15 +1,29 @@
 #' Clean text of tweets
 #'
 #' Removes from the text, users mentions, hashtags, urls and media.
-#' Some urls or other text might remain if it is not recognized as an entity by the API.
+#' Some urls or other text might remain if it is not recognized as an entity by
+#' the API.
 #' @param x Tweets
 #' @param clean Type of elements to be removed.
 #' @return A vector with the text without the entities selected
 #' @export
+#' @examples
+#' if (auth_has_default()) {
+#' tweets <- search_tweets("weather")
+#' tweets
+#'
+#' # tweets
+#' clean_tweets(tweets)
+#' }
 clean_tweets <- function(x, clean = c("users", "hashtags", "urls", "media")) {
+  if (is.character(x)) {
+    stop("You should provide tweets with all the users and hashtags information",
+         call. = FALSE)
+  }
+
   tweets <- nrow(x)
   final_text <- vector(mode = "character", length = tweets)
-  clean <- match.arg(clean)
+  clean <- match.arg(clean, several.ok = TRUE)
   for (tweet_n in seq_len(tweets)) {
     text <- x$full_text[tweet_n]
 
@@ -17,29 +31,37 @@ clean_tweets <- function(x, clean = c("users", "hashtags", "urls", "media")) {
     end <- vector("numeric")
     if ("users" %in% clean) {
       i <- x$entities[[tweet_n]][["user_mentions"]][["indices"]]
-      start <- c(start, i_type(i))
-      end <- c(end, i_type(i, type = "end"))
+      if (!is.null(i)) {
+        start <- c(start, i_type(i))
+        end <- c(end, i_type(i, type = "end"))
+      }
     }
 
     if ("hashtags" %in% clean) {
       hashtags <- x$entities[[tweet_n]][["hashtags"]][["indices"]]
-      i <- as.data.frame(do.call("rbind", hashtags))
+      if (!is.null(hashtags)) {
+        i <- as.data.frame(do.call("rbind", hashtags))
 
-      start <- c(start, i_type(i))
-      end <- c(end, i_type(i, type = "end"))
+        start <- c(start, i_type(i))
+        end <- c(end, i_type(i, type = "end"))
+      }
     }
 
     if ("media" %in% clean) {
       media <- x$entities[[tweet_n]][["media"]][["indices"]]
-      i <- as.data.frame(do.call("rbind", media))
-      start <- c(start, i_type(i))
-      end <- c(end, i_type(i, type = "end"))
+      if (!is.null(media)) {
+        i <- as.data.frame(do.call("rbind", media))
+        start <- c(start, i_type(i))
+        end <- c(end, i_type(i, type = "end"))
+      }
     }
 
     if ("urls" %in% clean) {
       i <- x$entities[[tweet_n]][["urls"]][["indices"]]
-      start <- c(start, i_type(i))
-      end <- c(end, i_type(i, type = "end"))
+      if (!is.null(i)) {
+        start <- c(start, i_type(i))
+        end <- c(end, i_type(i, type = "end"))
+      }
     }
     nothing <- start != 0
     start <- start[nothing]
@@ -53,7 +75,7 @@ clean_tweets <- function(x, clean = c("users", "hashtags", "urls", "media")) {
     }
   }
   # Remove tags about retweets
-  trimws(gsub("RT: ", final_text, replacement = "", fixed = TRUE))
+  gsub("RT : ", final_text, replacement = "", fixed = TRUE)
 }
 
 i_type <- function(x, type = "start") {
@@ -67,6 +89,8 @@ repaste <- function(text, start, stop) {
   stopifnot(length(text) == 1)
   text <- strsplit(text, "")[[1]]
   remove <- vector(mode = "numeric")
+  # Account for index starting at 0 in Twitter API output
+  start <- start + 1
   for (s_p in seq_along(start)) {
     position_str <- seq(from = start[s_p], to = stop[s_p])
     remove <- c(remove, position_str)
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,11 +1,12 @@
 # Use conditionally testthat
-tests_are_doable <- requireNamespace("rtweet", quietly = TRUE) && 
-  requireNamespace("testthat", quietly = TRUE) && 
+tests_are_doable <- requireNamespace("rtweet", quietly = TRUE) &&
+  requireNamespace("testthat", quietly = TRUE) &&
   requireNamespace("vcr", quietly = TRUE)
+
 if (tests_are_doable) {
   library(testthat)
   library(rtweet)
-  
+
   test_check("rtweet")
-  
+
 }
