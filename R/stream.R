4185d6460ccb6b7c4bb1f90bf7ae2484cafcbf72
Lluís <lluis.revilla@gmail.com>
2022-10-16 12:40:21 +0200

    Improved creation of classed tweets from streaming

--- R/stream.R
+++ R/stream.R
@@ -1,31 +1,31 @@
 #' Collect a live stream of Twitter data
-#' 
-#' @description 
+#'
+#' @description
 #' Streams public statuses to a file via one of the following four methods:
-#' 
+#'
 #' 1. Sampling a small random sample of all publicly available tweets
 #' 2. Filtering via a search-like query (up to 400 keywords)
 #' 3. Tracking via vector of user ids (up to 5000 user_ids)
 #' 4. Location via geo coordinates (1-360 degree location boxes)
-#' 
+#'
 #' Learn more in `vignette("stream", package = "rtweet")`
-#' 
+#'
 #' @inheritParams lookup_users
 #' @param q Query used to select and customize streaming collection
 #'   method.  There are four possible methods:
-#'   
-#'   1. The default, `q = ""`, returns a small random sample of all 
-#'      publicly available Twitter statuses. 
-#'   2. To filter by keyword, provide a comma separated character string with 
-#'      the desired phrase(s) and keyword(s). 
-#'   3. Track users by providing a comma separated list of user IDs or 
-#'      screen names. 
-#'   4. Use four latitude/longitude bounding box points to stream by geo 
-#'      location. This must be provided via a vector of length 4, e.g., 
+#'
+#'   1. The default, `q = ""`, returns a small random sample of all
+#'      publicly available Twitter statuses.
+#'   2. To filter by keyword, provide a comma separated character string with
+#'      the desired phrase(s) and keyword(s).
+#'   3. Track users by providing a comma separated list of user IDs or
+#'      screen names.
+#'   4. Use four latitude/longitude bounding box points to stream by geo
+#'      location. This must be provided via a vector of length 4, e.g.,
 #'      `c(-125, 26, -65, 49)`.
 #' @param timeout Integer specifying number of seconds to stream tweets for.
 #'   Stream indefinitely with `timeout = Inf`.
-#'    
+#'
 #'   The stream can be interrupted at any time, and `file_name` will still be
 #'   valid file.
 #' @param file_name Character with name of file. If not specified,
@@ -45,17 +45,17 @@
 #' rstats1
 #'
 #' # Download another 10s worth of data to the same file
-#' rstats2 <- stream_tweets("#rstats", timeout = 10, file_name = "rstats.json", 
+#' rstats2 <- stream_tweets("#rstats", timeout = 10, file_name = "rstats.json",
 #'                          append = TRUE)
 #'
 #' # stream tweets about continental USA for 10 seconds
-#' usa <- stream_tweets(location = lookup_coords("usa"), file_name = "usa.json", 
+#' usa <- stream_tweets(location = lookup_coords("usa"), file_name = "usa.json",
 #'                      timeout = 10)
-#' 
+#'
 #' }
 #' @return A tibble with one row per tweet
 #' @export
-#' @references 
+#' @references
 #' Stream: <https://developer.twitter.com/en/docs/twitter-api/v1/tweets/sample-realtime/api-reference/get-statuses-sample>
 #' Filter: <https://developer.twitter.com/en/docs/twitter-api/v1/tweets/filter-realtime/api-reference/post-statuses-filter>
 stream_tweets <- function(q = "",
@@ -75,11 +75,11 @@ stream_tweets <- function(q = "",
   prep <- stream_prep(token, q, ...)
   stream <- curl::curl(prep$url, handle = prep$handle)
 
-  quiet_interrupt(download_from_stream(stream, output, 
+  quiet_interrupt(download_from_stream(stream, output,
     timeout = timeout,
     verbose = verbose
   ))
-  
+
   if (parse) {
     df <- parse_stream(file(file_name))
   } else {
@@ -93,7 +93,7 @@ download_from_stream <- function(stream, output, append = TRUE, timeout = 10, ve
   }
   stopifnot(is.numeric(timeout), timeout > 0)
   stop_time <- Sys.time() + timeout
-  
+
   n_seen <- 0
   if (verbose) {
     pb <- progress::progress_bar$new(
@@ -103,9 +103,9 @@ download_from_stream <- function(stream, output, append = TRUE, timeout = 10, ve
     )
   }
 
-  open(stream, "rb") 
+  open(stream, "rb")
   withr::defer(close(stream), current_env(), priority = "first")
-  
+
   open(output, if (append) "ab" else "b")
   withr::defer(close(output), current_env(), priority = "last")
 
@@ -119,12 +119,12 @@ download_from_stream <- function(stream, output, append = TRUE, timeout = 10, ve
       Sys.sleep(0.25)
       next
     }
-  
+
     text <- rawToChar(buf)
     lines <- whole_lines(text, lines$fragment)
-    
+
     # only keep tweets
-    # TODO: process more messages from 
+    # TODO: process more messages from
     # https://developer.twitter.com/en/docs/twitter-api/v1/tweets/filter-realtime/overview
     json <- lapply(lines$lines, jsonlite::fromJSON)
     is_tweet <- vapply(json, function(x) has_name(x, "created_at"), logical(1))
@@ -133,10 +133,10 @@ download_from_stream <- function(stream, output, append = TRUE, timeout = 10, ve
     if (verbose) {
       pb$tick(length(buf), tokens = list(n = n_seen))
     }
-  
+
     writeLines(lines$lines[is_tweet], output, useBytes = TRUE)
   }
-  
+
   if (verbose) {
     cat("\n")
   }
@@ -151,7 +151,7 @@ quiet_interrupt <- function(code) {
 whole_lines <- function(text, fragment = "") {
   lines <- strsplit(text, "\r\n")[[1]]
   lines[[1]] <- paste0(fragment, lines[[1]])
-    
+
   n <- length(lines)
   complete <- grepl("\r\n$", text)
   if (!complete) {
@@ -160,7 +160,7 @@ whole_lines <- function(text, fragment = "") {
   } else {
     fragment <- ""
   }
-  
+
   # Drop empty keep-alive lines
   lines <- lines[lines != ""]
 
@@ -170,7 +170,7 @@ whole_lines <- function(text, fragment = "") {
 stream_prep <- function(token, q = "", ..., filter_level = "none") {
   token <- check_token(token)
   stopifnot(is.atomic(q) || inherits(q, "coords"))
-  
+
   if (identical(q, "")) {
     path <- "1.1/statuses/sample.json"
     params <- NULL
@@ -184,11 +184,11 @@ stream_prep <- function(token, q = "", ..., filter_level = "none") {
     warning("Detected a long parameter, this query will probably fail.", call. = FALSE)
   }
 
-  url <- httr::modify_url("https://stream.twitter.com", 
-    path = path, 
+  url <- httr::modify_url("https://stream.twitter.com",
+    path = path,
     query = params
   )
-    
+
   handle <- curl::new_handle()
   curl::handle_setheaders(handle, .list = token$sign("GET", url)$headers)
 
@@ -205,7 +205,7 @@ stream_params <- function(stream, ...) {
   } else {
     params <- list(track = stream, ...)
   }
-  
+
   params
 }
 
@@ -219,50 +219,57 @@ is_user_ids <- function(x) {
 
 
 #' Parser of stream
-#' 
+#'
 #' Converts Twitter stream data (JSON file) into parsed data frame.
 #' @param path Character, name of JSON file with data collected by
 #'   [stream_tweets()].
 #' @param ... Unused, keeping it for back compatibility.
 #' @export
 #' @seealso `stream_tweets()`
-#' @examples 
+#' @examples
 #' \dontrun{
 #' stream_tweets(timeout = 1, file_name = "stream.json", parse = FALSE)
 #' parse_stream("stream.json")
 #' }
 parse_stream <- function(path, ...) {
   if (...length() > 0) {
-    lifecycle::deprecate_soft("1.0.0", "parse_steam(...)", 
+    lifecycle::deprecate_soft("1.0.0", "parse_steam(...)",
                               details = c("Parameters ignored"))
   }
-  
+
   if (!is(path, "file")) {
     path <- file(path)
   }
-  
+
   tweets <- jsonlite::stream_in(path, pagesize = 1, verbose = FALSE)
   tweets <- tweet(tweets)
   tweets$created_at <- format_date(tweets$created_at)
-  
-  if (has_name_(tweets, "user")) {
-    users <- do.call(rbind, tweets[["user"]])
-    tweets <- tweets[!colnames(tweets) %in% "user"]
-  } else {
-    users <- user(NULL)
+
+  tweets0 <- tweet(NULL)[0, ]
+  if (length(tweets) != 0) {
+    tweets <- tweets[, colnames(tweets0)]
   }
-  if (nrow(tweets) == 0 || is.null(tweets)) {
-    tweets <- tweet(NULL)[0, ]
-    users <- users[0, ]
+
+
+  users <- user(NULL)[0, ]
+  if (has_name_(tweets, "user") && length(tweets$user) != 0 && all(lengths(tweets$user) != 0)) {
+    users <- do.call(rbind, tweets[["user"]])[, order(colnames(users))]
   }
-  structure(tweets, users = users)
+
+  tweets <- tweets[!colnames(tweets) %in% "user"]
+  users <- tibble::as_tibble(users)
+  tweets <- tibble::as_tibble(tweets)
+
+  out <- structure(tweets, users = users)
+  class(out) <- c("tweets", class(out))
+  out
 }
 
 
 # Deprecated -----------------------------------------------------------------
 #' A more robust version of stream_tweets
 #'
-#' @description 
+#' @description
 #' `r lifecycle::badge("deprecated")`
 #' Please use [stream_tweets()] instead.
 #'
--- tests/testthat/test-stream.R
+++ tests/testthat/test-stream.R
@@ -6,11 +6,11 @@ test_that("stream_tweets returns tweets data", {
   }
   path <- tempfile()
   x1 <- stream_tweets(timeout = 1, file_name = path, verbose = FALSE)
-  expect_s3_class(x1, "data.frame")
+  expect_s3_class(x1, "tweets")
   expect_error(ud <- users_data(x1), NA)
 
   stream_tweets(timeout = 1, file_name = path, verbose = FALSE, parse = FALSE)
-  
+
   x2 <- parse_stream(path)
   expect_true(nrow(x2) > nrow(x1))
 })
