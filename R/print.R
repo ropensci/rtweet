368bda8876e6e7ad4f05f96cb8fc626baf117994
Lluís <lluis.revilla@gmail.com>
2022-12-08 23:41:17 +0100

    Provide informative print methods

---
+++ R/print.R
@@ -0,0 +1,19 @@
+#' @export
+print.post_tweet <- function(x, ...) {
+  if (httr::status_code(pt) != 200L) {
+    cat("Your message has not been posted!", call. = FALSE)
+  }
+  ct <- httr::content(x)
+  cat("Tweet", ct$id_str, "posted from", ct$user$screen_name,  "\nText:", ct$text)
+}
+
+#' @export
+print.tweet_deleted <- function(x, ...) {
+  if (httr::status_code(pt) != 200L) {
+    cat("Your message has not been deleted!", call. = FALSE)
+  }
+  ct <- httr::content(x)
+  created <- format_date(ct$created_at)
+  cat("Tweet", ct$id_str, "deleted from", ct$user$screen_name,
+      "\nCreated at", as.character(created), ":", ct$text)
+}
