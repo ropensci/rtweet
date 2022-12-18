837c17b59d0750f286cf92a680c9486327449e6d
Lluís <lluis.revilla@gmail.com>
2022-12-18 18:01:37 +0100

    Add a test

--- tests/testthat/test-stream.R
+++ tests/testthat/test-stream.R
@@ -10,3 +10,23 @@ test_that("stream_tweets returns tweets data", {
   x2 <- parse_stream(path)
   expect_true(nrow(x2) > nrow(x1))
 })
+
+
+test_that("", {
+  skip_if_offline()
+  if (!"RTAA" %in% auth_list()) {
+    skip("Requires different authentication")
+  }
+  auth_as("RTAA")
+  new_rule <- stream_add_rule(list(value = "#rstats", tag = "rstats"))
+  # Open filtered streaming connection for 30s
+  tmp <- tempfile()
+  expect_error({
+    f <- filtered_stream(file = tmp, timeout = 30, parse = FALSE)
+    f2 <- filtered_stream(file = tmp, timeout = 30, parse = FALSE)
+  }, NA)
+  # Remove rule
+  stream_rm_rule(ids(new_rules))
+  # Open random streaming connection
+  sample_stream(file = tempfile(), timeout = 3)
+})
