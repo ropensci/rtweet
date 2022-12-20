2c77e9568b0335ce6c74b53dd5b3b6ffcc4d1858
Lluís <lluis.revilla@gmail.com>
2022-12-20 22:22:32 +0100

    Change to entities

--- R/entities.R
+++ R/entities.R
@@ -1,9 +1,14 @@
 #' Extract methods
 #'
 #' Extract entities of the tweets linked to a tweet id.
+#'
+#' The position of where does this occur is not provided.
 #' @param x A tweets object of the rtweet package.
+#' @param entity A entity to extract data from.
+#' @param ... Other possible arguments currently ignored.
 #' @return Some information about those entities and the tweet id it comes from.
-#' @name helpers
+#' for users mentions the ids of the mentioned users are "user_id", "user_id_str" (not id_str)
+#' @export
 #' @examples
 #' if (auth_has_default()) {
 #'   statuses <- c(
@@ -14,86 +19,41 @@
 #'
 #'   ## lookup tweets data for given statuses
 #'   tw <- lookup_tweets(statuses)
-#'   urls(tw)
+#'   entity(tw, "urls")
 #'
 #' }
-NULL
+entity <- function(x, entity, ...) {
+  UseMethod("entity")
+}
+
+#' @export
+entity.default <- function(x, entity, ...) {
+  is(x, "tweets")
+  entity <- match.arg(entity, c("urls", "hashtags", "symbols",
+                                "user_mentions", "media"))
+  l <- lapply(x, function(x){x[[entity]]})
+  l
+}
 
 
+#' @export
+entity.tweets <- function(x, entity) {
+  entity <- match.arg(entity, c("urls", "hashtags", "symbols",
+                                "user_mentions", "media"))
 
-entity <- function(x, name = "urls") {
+  out <- .entity(x, entity)
+
+  if (entity == "user_mentions") {
+    colnames(out) <- c("id_str", "screen_name", "name", "user_id", "user_id_str")
+  }
+  out
+}
+
+.entity <- function(x, name = "urls") {
   ids <- ids(x)
-  ent <- lapply(x$entities, function(x) {x[[name]]})
+  ent <- lapply(x$entities, function(x) {l_minus(x[[name]], "indices")})
   n_ent <- vapply(ent, NROW, numeric(1L))
   ids <- rep(ids, n_ent)
   ent <- do.call(rbind, ent)
   cbind.data.frame(id_str = ids, ent)
-}
-
-#' @export
-#' @describeIn helpers Extract urls.
-urls <- function(x, ...) {
-  UseMethod("urls")
-}
-
-#' @export
-#' @method urls tweets
-urls.tweets <- function(x, ...) {
-  out <- entity(x, "urls")
-  out[, c("url", "expanded_url", "display_url")]
-}
-
-#' @export
-#' @describeIn helpers Extract hashtags.
-hashtags <- function(x, ...) {
-  UseMethod("hashtags")
-}
-
-#' @export
-#' @method hashtags tweets
-hashtags.tweets <- function(x, ...) {
-  out <- entity(x, "hashtags")
-  out[, c("id_str", "text")]
-}
-
-#' @export
-#' @describeIn helpers Extract symbols.
-symbols <- function(x, ...) {
-  UseMethod("symbols")
-}
-
-#' @export
-#' @method symbols tweets
-symbols.tweets <- function(x, ...) {
-  out <- entity(x, "symbols")
-  out[, c("id_str", "text")]
-}
-
-#' @export
-#' @describeIn helpers Extract user_mentions.
-user_mentions <- function(x, ...) {
-  UseMethod("user_mentions")
-}
-
-#' @export
-#' @method user_mentions tweets
-user_mentions.tweets <- function(x, ...) {
-  out <- entity(x, "user_mentions")
-  out[, c("id_str", "screen_name", "name", "id", "id_str")]
-  colnames(out) <- c("id_str", "screen_name", "name", "user_id", "user_id_str")
-  out
-}
-
-#' @export
-#' @describeIn helpers Extract media.
-media <- function(x, ...) {
-  UseMethod("media")
-}
-
-#' @export
-#' @method media tweets
-media.tweets <- function(x, ...) {
-  out <- entity(x, "media")
-  out[, c("id_str", "media_url", "media_url_https", "url", "display_url",
-          "expanded_url", "type", "ext_alt_text")]
 }
