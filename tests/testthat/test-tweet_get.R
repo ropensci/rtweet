7b7753bd613303bc48f831284218777f30ca19cd
Lluís <lluis.revilla@gmail.com>
2023-02-25 22:28:16 +0100

    Update test to parse = TRUE

--- tests/testthat/test-tweet_get.R
+++ tests/testthat/test-tweet_get.R
@@ -2,15 +2,16 @@ test_that("get_tweet works", {
   skip_if_offline()
   testing_with_authentication("bearer_testing_app")
   gt1 <- tweet_get("567053242429734913", parse = FALSE)
-  expect_s3_class(gt1, "Twitter_resp")
-  gt2 <- tweet_get(c("567053242429734913", "567053242429734913"), parse = FALSE)
-  expect_s3_class(gt2, "Twitter_resp")
-  expect_length(gt2, 2)
+  expect_true(inherits(gt1, "list"))
+  gt2 <- tweet_get(c("567053242429734913", "567053242429734913"), parse = TRUE)
+  expect_s3_class(gt2, "page")
+  expect_length(gt2, 3)
   gt2_parsed <- tweet_get(c("567053242429734913", "567053242429734913"),
                           parse = TRUE)
   expect_s3_class(gt2_parsed, "data.frame")
   expect_equal(dim(gt2_parsed), c(2, 3))
 })
+
 test_that("get_tweets works with expansions and fields", {
   skip_if_offline()
   testing_with_authentication("bearer_testing_app")
