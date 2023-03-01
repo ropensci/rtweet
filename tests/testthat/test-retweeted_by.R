b5c133e0805948e09d8f6f7c3a07d242eeaa3e41
Lluís <lluis.revilla@gmail.com>
2023-03-01 02:00:10 +0100

    Minor tweaks to perform well

--- tests/testthat/test-retweeted_by.R
+++ tests/testthat/test-retweeted_by.R
@@ -2,8 +2,8 @@ test_that("retweeted_by works", {
   skip_if_offline()
   testing_with_authentication("bearer_testing_app")
   # Pinned tweets where deleted so there will be errors
-  rb <- tweet_retweeted_by("567053242429734913", parse = FALSE, expansions = NULL,
-                     fields = NULL)
-
+  expect_error(tweet_retweeted_by("567053242429734913", parse = FALSE), NA)
+  rb <- tweet_retweeted_by("567053242429734913", parse = TRUE)
+  expect_s3_class(rb, "data.frame")
 
 })
