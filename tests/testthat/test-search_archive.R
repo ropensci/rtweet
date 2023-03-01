c374ab271cbc5bbf76bfe7bf50b3e1c31c3b1636
Lluís <lluis.revilla@gmail.com>
2023-03-01 01:58:57 +0100

    Standarization towards n instead of max_results

--- tests/testthat/test-search_archive.R
+++ tests/testthat/test-search_archive.R
@@ -1,13 +1,13 @@
 test_that("search_archive works", {
-  testing_with_authentication("academic_access")
-  expect_error(sa <- search_archive("#rtweet", parse = FALSE, max_results = 10), NA)
+  testing_with_authentication("academic_dev_oauth2")
+  sa <- expect_error(sa <- tweet_search_all("#rtweet", parse = FALSE, n = 10), NA)
   expect_length(sa$data, 10)
   expect_named(sa, c("data", "meta"))
 })
 
 test_that("search_recent works", {
-  testing_with_authentication("academic_access")
-  expect_error(sa <- search_recent("#rtweet", parse = FALSE, max_results = 10), NA)
+  testing_with_authentication("academic_dev_oauth2")
+  expect_error(sa <- tweet_search_recent("#rtweet", parse = FALSE, n = 10), NA)
   expect_length(sa$data, 10)
   expect_named(sa, c("data", "meta"))
 })
