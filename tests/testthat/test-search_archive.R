c0fb8a84cd87e05bd67d560901b9f2a81b6d0b37
Lluís <lluis.revilla@gmail.com>
2023-03-11 22:53:14 +0100

    Test with bearer

--- tests/testthat/test-search_archive.R
+++ tests/testthat/test-search_archive.R
@@ -1,14 +1,18 @@
 test_that("search_archive works", {
   skip_on_cran()
+  testing_with_authentication("bearer_academic_dev")
   sa <- expect_error(tweet_search_all("#rtweet", parse = FALSE, n = 10), NA)
   y <- auth_get()
   expect_length(sa[[1]]$data, 10)
   expect_named(sa[[1]], c("data", "meta"))
+  auth_as()
 })
 
 test_that("search_recent works", {
   skip_on_cran()
+  testing_with_authentication("bearer_testing_app")
   expect_error(sa <- tweet_search_recent("#rtweet", parse = FALSE, n = 10), NA)
   expect_length(sa[[1]]$data, 10)
   expect_named(sa[[1]], c("data", "meta"))
+  auth_as()
 })
