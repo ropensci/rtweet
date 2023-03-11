6e3b932bfe38c5c2c9019f4518742e8a52d63254
Lluís <lluis.revilla@gmail.com>
2023-03-11 13:32:56 +0100

    Improve scope checking

--- R/scopes.R
+++ R/scopes.R
@@ -25,7 +25,6 @@ get_scopes <- function(token, call = caller_env()) {
   strsplit(token$scope, " ")[[1]]
 }
 
-# These functions can be simplified:
 check_scopes <- function(scopes, required = NULL, call = caller_env()) {
   if (is.null(required)) {
     diff <- setdiff(scopes, all_scopes)
@@ -37,27 +36,18 @@ check_scopes <- function(scopes, required = NULL, call = caller_env()) {
   }
   missing <- setdiff(required, scopes)
   if (length(missing) != 0) {
-    msg <- paste0("This endpoint requires missing ",
-                  paste0(sQuote(missing), collapse = ", "), " scopes")
+    msg <- c("This endpoint requires missing scopes.",
+             paste0("Authenticate with scopes:", paste0(sQuote(missing), collapse = ", ")))
     abort(msg, call = call)
   }
   TRUE
 }
 
-check_scopes_token <- function(token, required) {
-  stopifnot(check_scopes(required))
+check_scopes_token <- function(token, required, call = caller_env()) {
   if (!auth_is_pkce(token)) {
     return(TRUE)
   }
-  scopes <- get_scopes(token)
-  if (is.null(scopes)) {
-    abort(c("The authentication used must have scopes.",
-            i = paste0("Authenticate with scopes:",
-                       paste0(sQuote(required), collapse = ", "))))
-  } else if (!check_scopes(scopes, required)) {
-    missing_scopes <- setdiff(required, scopes)
-    abort(c("Missing scopes for this endpoint:",
-            i = paste0("Authenticate with:", sQuote(missing_scopes))))
-  }
+  check_scopes(required, call)
+  check_scopes(get_scopes(token), required, call)
   TRUE
 }
