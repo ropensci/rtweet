83e59346b459ada7b7a70e122d43d36320cc4cd5
Lluís <lluis.revilla@gmail.com>
2023-03-11 11:34:51 +0100

    Fix test

--- tests/testthat/test-search_archive.R
+++ tests/testthat/test-search_archive.R
@@ -1,15 +1,14 @@
 test_that("search_archive works", {
   skip_on_cran()
-  sa <- expect_error(sa <- tweet_search_all("#rtweet", parse = FALSE, n = 10), NA)
+  sa <- expect_error(tweet_search_all("#rtweet", parse = FALSE, n = 10), NA)
   y <- auth_get()
-  print(y$token)
-  expect_length(sa$data, 10)
-  expect_named(sa, c("data", "meta"))
+  expect_length(sa[[1]]$data, 10)
+  expect_named(sa[[1]], c("data", "meta"))
 })
 
 test_that("search_recent works", {
   skip_on_cran()
   expect_error(sa <- tweet_search_recent("#rtweet", parse = FALSE, n = 10), NA)
-  expect_length(sa$data, 10)
-  expect_named(sa, c("data", "meta"))
+  expect_length(sa[[1]]$data, 10)
+  expect_named(sa[[1]], c("data", "meta"))
 })
