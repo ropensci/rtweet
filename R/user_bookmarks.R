c12b8a2f237c7ae55bffd9d42df32c03ebd5ef24
Lluís <lluis.revilla@gmail.com>
2023-03-12 23:42:35 +0100

    Change defaults to return minimal data and document better the returned fields

--- R/user_bookmarks.R
+++ R/user_bookmarks.R
@@ -9,16 +9,22 @@
 #' @param token This endpoint only accept a OAuth2.0 authentication (can be
 #' created via [rtweet_oauth2()]).
 #' @inheritParams sample_stream
-#' @seealso [rtweet_oauth2()]
+#' @seealso [rtweet_oauth2()], [user_self()]
 #' @export
-#' @returns The tweets.
+#' @returns A data.frame with the user information of who is following the list:
+#' edit_history_tweet_ids, id and text.
+#' Other information depends on the `expansions` and `fields` requested.
+#' Accepted values are:
+#' - Expansions: `set_expansions(list = NULL)`.
+#' - Fields: `set_fields(list = NULL)`.
 #' @references <https://developer.twitter.com/en/docs/twitter-api/tweets/bookmarks/api-reference/get-users-id-bookmarks>
 #' @examples
 #' if (FALSE) {
 #'    # Requires token_oa2
+#'    # Requires knowing your own user id
 #'    ub <- user_bookmarks("123456789", parse = FALSE, n = Inf, token = token_oa2)
 #' }
-user_bookmarks <- function(id, n = 100, ..., expansions = NA, fields = NA,
+user_bookmarks <- function(id, n = 100, ..., expansions = NULL, fields = NULL,
                            parse = TRUE, token = NULL, verbose = FALSE) {
   parsing(parse)
   if (!is_logical(verbose)) {
@@ -27,7 +33,7 @@ user_bookmarks <- function(id, n = 100, ..., expansions = NA, fields = NA,
   max_results <- check_interval(n, 1, formals()$n)
   n_pages <- ceiling(n / max_results)
   expansions <- check_expansions(arg_def(expansions, set_expansions(list = NULL)))
-  fields <- check_fields(arg_def(fields, set_fields()), metrics = NULL)
+  fields <- check_fields(arg_def(fields, set_fields(list = NULL)), metrics = NULL)
   expansions_for_fields(expansions, fields)
   data <- c(list(expansions = expansions), fields, max_results = max_results, ...)
   data <- unlist(prepare_params(data), recursive = FALSE)
