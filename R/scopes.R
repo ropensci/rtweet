f9c668c3c6f6903bdaf0bb01104a57c87d77bd75
Lluís <lluis.revilla@gmail.com>
2023-03-16 22:54:30 +0100

    Improve function to set tokens

--- R/scopes.R
+++ R/scopes.R
@@ -1,23 +1,46 @@
-all_scopes <- c("tweet.read", "tweet.write", "tweet.moderate.write", "users.read",
-                "follows.read", "follows.write", "offline.access", "space.read",
-                "mute.read", "mute.write", "like.read", "like.write", "list.read",
-                "list.write", "block.read", "block.write", "bookmark.read", "bookmark.write"
+all_scopes <- c("tweet.read", "users.read", "follows.read", "space.read",
+                "mute.read", "like.read", "block.read", "bookmark.read",
+                "list.read",
+                "tweet.write", "tweet.moderate.write", "follows.write", "offline.access", "mute.write", "like.write",
+                "list.write",  "block.write",  "bookmark.write"
 )
 
+endpoints <- c("tweet", "users", "follows", "space", "mute", "like", "list", "block", "bookmark")
 
-#' Scopes of the OAuth client
+#' Scopes of the OAuth2 token
 #'
-#' Permissions given to a client of a Twitter account.
+#' Permissions given to a token of a Twitter account.
+#' By default it allows everything.
 #' @export
-#' @param token An authentication mechanism. Preferably set via [auth_as()]
-#' @returns A character with all the possible scopes.
+#' @param read Allow to read.
+#' @param write Allow to write/manage?
+#' @param tweet_moderate Allow to hide and unhide replies to your Tweets.
+#' @param regenerate Allow to use the token for more than 2 hours.
+#' @returns A character with all the possible scopes or those allowed.
+#' @references <https://developer.twitter.com/en/docs/authentication/oauth-2-0/authorization-code>
 #' @examples
-#' set_scopes(NULL)
-set_scopes <- function(token = NULL) {
-  if (is.null(token)) {
-    return(all_scopes)
+#' set_scopes()
+set_scopes <- function(read = TRUE, write = TRUE, tweet_moderate = TRUE, regenerate = TRUE) {
+  scopes <- c()
+  if (isTRUE(read) && isTRUE(write)) {
+    scopes <- c(scopes, all_scopes)
   }
-  get_scopes(token)
+  if (isTRUE(read) && is.null(endpoints)) {
+    scopes <- all_scopes[endsWith(all_scopes, ".read")]
+  }
+  if (isTRUE(write) && is.null(endpoints)) {
+    scopes <- all_scopes[endsWith(all_scopes, ".write")]
+  } else if (isTRUE(write)) {
+
+  }
+  if (isTRUE(tweet_moderate)) {
+    scopes <- c(scopes, "tweet.moderate")
+  }
+  # To allow to regenerate the token we need to provide the offline.access:
+  if (isTRUE(regenerate)) {
+    scopes <- c(scopes, "offline.access")
+  }
+  scopes
 }
 
 get_scopes <- function(token, call = caller_env()) {
